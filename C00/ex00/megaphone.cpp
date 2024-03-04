3df6e3e0e7523a919010022fdd91356a6eadbf80
YounesMoukhlij <123767564+YounesMoukhlij@users.noreply.github.com>
2024-03-04 23:37:02 +0100

    only ex01 c00

---
+++ C00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (i < ac)
+		{
+			j = 0;
+			while (av[i][j])
+			{
+				if (av[i][j] >= 'a' && av[i][j] <= 'z')
+					av[i][j] -= 32;
+				std::cout << av[i][j];
+				j++;
+			}
+			if (i < ac - 1)
+				std::cout << " ";
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
