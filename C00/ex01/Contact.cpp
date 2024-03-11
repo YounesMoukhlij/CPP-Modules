6721efc5e12c7ffbbb9b6f759b293391e080dee3
Younes Moukhlij <youmoukh@e3r4p16.1337.ma>
2024-03-11 14:28:15 +0000

    new

--- C00/ex01/Contact.cpp
+++ C00/ex01/Contact.cpp
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 14:27:53 by youmoukh          #+#    #+#             */
+/*   Updated: 2024/03/11 14:27:55 by youmoukh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "Contact.hpp"
 
@@ -8,6 +19,7 @@ void	Contact::full_fill_contact(int number)
 	int	i;
 
 	i = 0;
+	(void) number;
 	while (i++ < 5)
 	{
 		std::cout << Contact::to_use[i] << " :\n";
--- C00/ex01/program.cpp
+++ C00/ex01/program.cpp
@@ -1,17 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   program.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 14:28:05 by youmoukh          #+#    #+#             */
+/*   Updated: 2024/03/11 14:28:07 by youmoukh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
 int	main(void)
 {
 	PhoneBook	phonebook;
-	bool		flag;
+	int			flag;
 	std::string	cmd;
 
 	flag = 1;
 	phonebook.display_banner();
 	while (flag)
 	{
-		std::cout << "📞 - ";
+		std::cout << "📞 ~ ";
 		std::getline(std::cin, cmd);
 		if (cmd == "ADD")
 			phonebook.add_contact_to_list();
