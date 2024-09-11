eac80b011e07695329648bd870066c9aea5047b4
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-09-11 17:38:57 +0100

    commit with gacp

---
+++ C01/ex02/main.cpp
@@ -0,0 +1,27 @@
+# include <cstdlib>
+# include <iostream>
+# include <string>
+
+int	main(int ac, char **argv)
+{
+	(void)argv;
+	if (ac != 0x1)
+	{
+		std::cerr << "the program does not take any args!" << std::endl;
+		return (EXIT_FAILURE);
+	}
+
+	std::string		arg = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &arg;
+	std::string&	stringREF = arg;
+
+	std::cout << (&arg) << std::endl;
+	std::cout << (stringPTR) << std::endl;
+	std::cout << (&stringREF) << std::endl;
+
+	std::cout << arg << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+
+	return (EXIT_SUCCESS);
+}
