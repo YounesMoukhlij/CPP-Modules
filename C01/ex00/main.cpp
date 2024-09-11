42d85ffd1866e13002bfaf6c307776caacc065c4
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-09-11 17:07:39 +0100

    commit with gacp

--- C01/ex00/main.cpp
+++ C01/ex00/main.cpp
@@ -12,9 +12,14 @@ int	main(void)
 	std::string	input;
 
 	atexit(show);
-	Zombie *z = newZombie(input);
+
+	std::cout << "\033[42m Go to randomChump function\033[0m" << std::endl;
+	randomChump("STACK_ZOMBIE");
+	std::cout << "\033[41m back from randomChump function \033[0m" << std::endl;
+	std::cout << "\033[44m Go to Heap allocation\033[0m" << std::endl;
+	Zombie *z = newZombie("HEAP_ZOMBIE");
 	z->announce();
 	delete(z);
-	randomChump(|);
+	std::cout << "\033[46m Back from Heap allocation\033[0m" << std::endl;
 	return (0x0);
 }
