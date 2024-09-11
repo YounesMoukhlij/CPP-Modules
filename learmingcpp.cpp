dd34392abf75cf4a74aad928eaa5c12753003db1
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-09-11 17:39:48 +0100

    commit with gacp

--- learmingcpp.cpp
+++ learmingcpp.cpp
@@ -5,18 +5,5 @@
 
 int	main()
 {
-	int i = 0;
 
-	while (i <= 9)
-	{
-		if (i % 2 == 0)
-		{
-			printf("a3dad zawjiya * 2 == %d\n", i * 2);
-		}
-		else
-		{
-			printf("a3dad fardiya %d \n", i);
-		}
-		i++;
-	}
 }
