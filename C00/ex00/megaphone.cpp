4fef2003947ffbdeae126ed5d4cf165ec95f38b6
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-09-20 11:31:51 +0100

    by gacp

--- C00/ex00/megaphone.cpp
+++ C00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 10:59:05 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/08/11 11:09:52 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/09/19 13:32:09 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,43 +15,27 @@
 
 int main(int ac, char **av)
 {
-	int	i;
-	int	j;
+	int	i, j;
 
-	i = 1;
-	if (ac == 1)
+	i = 0x1;
+	if (ac == i)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
 		while (i < ac)
 		{
-			j = 0;
-<<<<<<< HEAD
-			std::string str(av[i]);
-			while (j < str.length())
-			{
-				std::cout<< std::tolower(str[j]);
-			}
-			// while (av[i][j])
-			// {
-			// 	if (av[i][j] >= 'a' && av[i][j] <= 'z')
-			// 		av[i][j] -= 32;
-			// 	std::cout << av[i][j];
-			// 	j++;
-			// }
-			// if (i < ac - 1)
-			// 	std::cout << " ";
-=======
+			j = 0x0;
 			std::string array(av[i]);
 			while (j < array.length())
 			{
 				std::cout << (char)std::toupper(array[j]);
 				j++;
 			}
->>>>>>> e505e91d5326fdbdb513e031df5e9fc779d5587b
+			if (i < ac - 0x1)
+				std::cout << " ";
 			i++;
 		}
 		std::cout << std::endl;
 	}
-	return (0);
+	return EXIT_SUCCESS;
 }
