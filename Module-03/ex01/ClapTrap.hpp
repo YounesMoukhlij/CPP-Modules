62aff449ae0bd54f08f67036c3b019d50e749e82
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-09-22 18:27:58 +0100

    by gacp

---
+++ Module-03/ex01/ClapTrap.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/22 15:56:09 by youmoukh          #+#    #+#             */
+/*   Updated: 2024/09/22 16:03:16 by youmoukh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+
+#include <iostream>
+
+class	ClapTrap
+{
+
+	private:
+		std::string			_name;
+		unsigned int		_hitPoints;
+		unsigned int		_energyPoints;
+		unsigned int		_attackDamage;
+
+	public:
+		ClapTrap();
+		ClapTrap( const std::string& name );
+		ClapTrap( const ClapTrap& origine );
+		ClapTrap& operator=(const ClapTrap& origine );
+		~ClapTrap();
+		
+		void	attack( const std::string& target );
+		void	takeDamage( unsigned int amount );
+		void	beRepaired( unsigned int amount );
+};
+
+#endif
---
+++ Module-03/ex01/ScavTrap.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/22 18:11:55 by youmoukh          #+#    #+#             */
+/*   Updated: 2024/09/22 18:18:15 by youmoukh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+	public :
+		~ScavTrap();
+        ScavTrap( const ScavTrap& origine );
+		ScavTrap( const std::string& name );
+		ScavTrap& operator=(const ScavTrap& orgine);
+		
+		void	guardGate();
+		void	attack( const std::string& target );
+};
+
+
+#endif
