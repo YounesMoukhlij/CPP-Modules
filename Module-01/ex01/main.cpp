a487e19558992c5d037ccbc7568b99b73040ed57
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-09-23 18:34:08 +0100

    by gacp

--- Module-01/ex01/main.cpp
+++ Module-01/ex01/main.cpp
@@ -6,17 +6,21 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 17:18:50 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/09/23 18:33:47 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/09/23 18:34:08 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+
+
 
 int main(int ac, char **av)
 {
 	(void) av;
 	int	number = 0x5;
 
+	atexit(low);
 	if (ac != 0x1)
 	{
 		std::cerr << "the program doesn't take any args!"  << std::endl;
