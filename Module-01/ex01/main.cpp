6e28e088ba7788cbdca39bf850f01ca91fa39bd5
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-09-23 18:33:06 +0100

    by gacp

--- Module-01/ex01/main.cpp
+++ Module-01/ex01/main.cpp
@@ -6,17 +6,11 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 17:18:50 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/09/23 18:32:48 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/09/23 18:33:00 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
-
-void print_name(int num, Zombie *z)
-{
-}
-    for (int i = 0x0; i < num; i++)
-		  z[i].announce();
 
 int main(int ac, char **av)
 {
@@ -29,8 +23,8 @@ int main(int ac, char **av)
 		return (0x1);
 	}
 	Zombie *z = zombieHorde(number, "younan");
-	
-	print_name(number, z);
+    for (int i = 0x0; i < num; i++)
+		  z[i].announce();
     delete [] z;
 	return (0x0);
 }
