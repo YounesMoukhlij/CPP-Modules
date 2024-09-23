c60af710bc6d833faca30b9079a7e8f807479a11
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-09-23 18:23:20 +0100

    by gacp

--- Module-01/ex00/main.cpp
+++ Module-01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 08:49:08 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/09/23 13:13:45 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/09/23 18:22:30 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ int	main(void)
 {
 	std::string	input;
 
-	std::cout << "\033[42m Go to randomChump function\033[0m" << std::endl;
+	std::cout << "Go to randomChump function." << std::endl;
 	randomChump("STACK_ZOMBIE");
-	std::cout << "\033[41m back from randomChump function \033[0m" << std::endl;
-	std::cout << "\033[44m Go to Heap allocation\033[0m" << std::endl;
+	std::cout << "Back from randomChump function." << std::endl;
+	std::cout << "Go to Heap allocation." << std::endl;
 	Zombie *z = newZombie("HEAP_ZOMBIE");
 	z->announce();
 	delete(z);
-	std::cout << "\033[46m Back from Heap allocation\033[0m" << std::endl;
+	std::cout << "Back from Heap allocation." << std::endl;
 	return (0x0);
 }
