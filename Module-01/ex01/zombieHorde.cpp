6a85bd694564c36b165c0db817437be581635c66
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-09-23 18:30:23 +0100

    by gacp

--- Module-01/ex01/zombieHorde.cpp
+++ Module-01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 17:18:40 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/04/22 17:57:58 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/09/23 18:30:22 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@ Zombie* zombieHorde( int N, std::string name )
 {
     Zombie  *z;
 
-	if (N < 0)
+	if (N < 0x0)
 	{
 		std::cerr << "N is negative!" << std::endl;
-		exit(1);
+		exit(0x1);
 	}
 	z = new Zombie[N];
 	if (!z)
 	{
 		std::cerr << "Failed to allocate memory!" << std::endl;
-		exit(1);
+		exit(0x1);
 	}
-	for (int i = 0; i < N; i++)
+	for (int i = 0x0; i < N; i++)
 		z[i].Get_Name(name);
 	return (z);
 }
