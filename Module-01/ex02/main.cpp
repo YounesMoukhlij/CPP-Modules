b285c1b8cc880eec11337b3fa23d455114a05527
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-09-24 16:43:24 +0100

    by gacp

--- Module-01/ex02/main.cpp
+++ Module-01/ex02/main.cpp
@@ -6,23 +6,21 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 08:50:24 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/09/17 08:50:25 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/09/23 18:38:18 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <cstdlib>
 # include <iostream>
-# include <string>
 
 int	main(int ac, char **argv)
 {
-	(void)argv;
+	(void) argv;
+
 	if (ac != 0x1)
 	{
 		std::cerr << "the program does not take any args!" << std::endl;
 		return (EXIT_FAILURE);
 	}
-
 	std::string		arg = "HI THIS IS BRAIN";
 	std::string*	stringPTR = &arg;
 	std::string&	stringREF = arg;
