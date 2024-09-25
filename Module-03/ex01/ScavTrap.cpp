e4b44eb4a2493bc599130aa4c49a7d5dbb768059
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-09-25 19:09:56 +0100

    by gacp

--- Module-03/ex01/ScavTrap.cpp
+++ Module-03/ex01/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 18:11:59 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/09/25 19:00:27 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/09/25 19:09:48 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,20 @@ ScavTrap::ScavTrap( const ScavTrap& origine )
 {
 	
 }
-ScavTrap::ScavTrap( const std::string& name )
+ScavTrap::ScavTrap( const std::string& name ) : ClapTrap(name)
 {
 
 }
-ScavTrap& ScavTrap::operator=(const ScavTrap& orgine)
+ScavTrap& ScavTrap::operator=(const ScavTrap& origine)
 {
-
+	*this = origine;
 }
 		
 void	ScavTrap::guardGate()
 {
-	
+	std::cout << name << "ScavTrap is now in Gate keeper mode." << std::endl;
 }
 
 void	ScavTrap::attack( const std::string& target )
 {
-	std::cout << "ScavTrap is now in Gate keeper mode." << std::endl;
 }
