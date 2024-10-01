981a7b55362aaa93067ca73498b08d7f821d57cb
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-01 17:13:59 +0100

    by gacp

--- Module-05/ex00/Bureaucrat.cpp
+++ Module-05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:37:59 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 17:08:39 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 17:13:56 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,7 @@ Bureaucrat::Bureaucrat(const Bureaucrat& origine)
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat& origine)
 {
 	if (this != &origine)
+		this = origine;
 	std::cout << "Bureaucrat copy assignement  Called" << std::endl;
 }
 
