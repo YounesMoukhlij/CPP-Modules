f56d5dcbc73e8a45c34cc86c3d4f783f17855515
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-01 16:26:53 +0100

    by gacp

--- Module-05/ex00/Bureaucrat.hpp
+++ Module-05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 16:26:08 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 16:26:52 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,26 +26,26 @@ class Bureaucrat
 		Bureaucrat& operator=(const Bureaucrat& origine);
 		~Bureaucrat();
 	
-   class GradeTooHighException : public std::exception
-   {
-    	public:
-    	    const char* what() const noexcept override
-			{
-    	        return "Grade is too high!";
-    	    }
-    };
-
-    class GradeTooLowException : public std::exception
-	{
-    	public:
-    	    const char* what() const noexcept override
-			{
-    	        return "Grade is too low!";
-    	    }
-    };
+   		class GradeTooHighException : public std::exception
 	
-		const std::string	getName() const;
-		int 				getGrade();
+   			public:
+   			    const char* what() const noexcept override
+					{
+   			        return "Grade is too high!";
+   			    }
+   		};
+
+   		class GradeTooLowException : public std::exception
+			{
+   			public:
+   			    const char* what() const noexcept override
+					{
+   			        return "Grade is too low!";
+   			    }
+   		};
+			
+				const std::string	getName() const;
+				int 				getGrade();
 	
 };
 
