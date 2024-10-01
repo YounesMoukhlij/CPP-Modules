509591b40705fa3e25279bb1683deab2733ab1ae
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-01 18:07:08 +0100

    by gacp

--- Module-05/ex01/Form.hpp
+++ Module-05/ex01/Form.hpp
@@ -6,8 +6,16 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 18:06:15 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 18:06:35 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 18:07:05 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma
+#pragma once
+
+#include <iostream>
+
+class From
+{
+	private :
+		const std::string _name
+};
