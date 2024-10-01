451d87bd044adea1860e78e8dd6bb08abc999c47
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-01 18:08:42 +0100

    by gacp

--- Module-05/ex01/Form.hpp
+++ Module-05/ex01/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 18:06:15 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 18:08:18 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 18:08:42 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,9 @@ class From
 		const				_gradeExecut;
 	public:
 		~Form();
-		~Form();
-		~Form();
+		Form();
+		Form(const Form& origine);
+		Form(const Form& origine);
+		Form&
 		
 };
