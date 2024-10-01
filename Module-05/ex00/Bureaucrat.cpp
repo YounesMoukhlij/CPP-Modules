80e491535d14c5eb27d055e71904a1514ea116b4
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-01 16:43:19 +0100

    by gacp

--- Module-05/ex00/Bureaucrat.cpp
+++ Module-05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:37:59 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 16:41:49 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 16:43:19 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,5 +74,6 @@ void	Bureaucrat::decrementGrade()
 
 std::ostream& operator<<(std::ostream& file, const Bureaucrat& bureaucrat)
 {
+	file 
 	
 }
