f8b9eb08c1d88579bbd78de00a61bafb670a0dc8
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-01 17:42:20 +0100

    by gacp

--- Module-05/ex00/main.cpp
+++ Module-05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:05 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 17:41:54 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 17:42:19 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ int main(void)
 	{
         std::cerr << "\033[32mException caught: " << o.what() << "\033[0m" <<std::endl;
     }
+
+    std::cout << "After incrementing: " << bob << std::endl;
 
     try
 	{
