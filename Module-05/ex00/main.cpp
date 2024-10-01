057272d7963bae378786bbda54fd9686db0b8021
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-01 13:06:26 +0100

    by gacp

--- Module-05/ex00/main.cpp
+++ Module-05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:05 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 13:06:03 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 13:06:25 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ int main(void)
 {
 	Bureaucrat Bur;
 	
-	
+	std::cout << Bur.getName() << " B"
 	return (EXIT_SUCCESS);
 }
