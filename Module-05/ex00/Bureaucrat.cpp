1080e5e6e99db2356941b1ad2e4ba49361f11849
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-01 12:47:55 +0100

    by gacp

--- Module-05/ex00/Bureaucrat.cpp
+++ Module-05/ex00/Bureaucrat.cpp
@@ -6,7 +6,31 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:37:59 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 12:37:59 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 12:47:43 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat Destructor Called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat()
+{
+	std::cout << "Bureaucrat Default Constructor Called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& origine)
+{
+	std::cout << "Bureaucrat Parametriezed Constructor Called" << std::endl;
+	*this = origine;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& origine)
+{
+	if (*this != origine)
+		*this = origine;
+
+}
--- Module-05/ex00/Bureaucrat.hpp
+++ Module-05/ex00/Bureaucrat.hpp
@@ -6,7 +6,24 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 12:38:01 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 12:44:51 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+# include <iostream>
+
+class Bureaucrat
+{
+	private:
+
+	public:
+	~Bureaucrat();
+	Bureaucrat();
+	Bureaucrat(const Bureaucrat& origine);
+	Bureaucrat& operator=(const Bureaucrat& origine);
+	
+};
+
+std::ostream& operator<<(std::ostream &f, std::string str);
