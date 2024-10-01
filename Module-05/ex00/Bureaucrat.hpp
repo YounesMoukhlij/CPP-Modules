11e8b1abdf014b90889a3dcd3e8a77bd75ba9084
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-01 16:26:25 +0100

    by gacp

--- Module-05/ex00/Bureaucrat.hpp
+++ Module-05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 15:59:10 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 16:26:08 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,8 +44,8 @@ class Bureaucrat
     	    }
     };
 	
-		const std::string getName() const;
-		int getGrade();
+		const std::string	getName() const;
+		int 				getGrade();
 	
 };
 
