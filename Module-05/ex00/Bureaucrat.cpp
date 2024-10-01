fc954425db45eb2439fdf91f783f592fdb245510
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-01 15:59:31 +0100

    by gacp

--- Module-05/ex00/Bureaucrat.cpp
+++ Module-05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:37:59 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 13:10:07 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 15:59:26 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ Bureaucrat::~Bureaucrat()
 }
 
 Bureaucrat::Bureaucrat()
+{
+	std::cout << "Bureaucrat Default Constructor Called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string name, int grade) : 
 {
 	std::cout << "Bureaucrat Default Constructor Called" << std::endl;
 }
--- Module-05/ex00/Bureaucrat.hpp
+++ Module-05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 15:59:01 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 15:59:10 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ class Bureaucrat
 		int					_grade;
 	public:
 		Bureaucrat();
-		Bureaucrat(const std::string name);
 		Bureaucrat(const Bureaucrat& origine);
+		Bureaucrat(const std::string name, int grade);
 		Bureaucrat& operator=(const Bureaucrat& origine);
 		~Bureaucrat();
 	
