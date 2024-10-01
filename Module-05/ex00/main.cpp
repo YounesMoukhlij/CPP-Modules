b246f7f2fd011a29d99457312f6dbfe1c730494f
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-01 13:07:08 +0100

    by gacp

--- Module-05/ex00/main.cpp
+++ Module-05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:05 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 13:06:46 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 13:07:05 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ int main(void)
 {
 	Bureaucrat Bur;
 	
-	std::cout << Bur.getName() << " Bureaucrat grade " << Bur.getGrade
+	std::cout << Bur.getName() << ", Bureaucrat grade " << Bur.getGrade() << "." << std::endl;
 	return (EXIT_SUCCESS);
 }
