a87634271c1fd4d892c528fdb29d55ffff613282
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-01 17:07:07 +0100

    by gacp

--- Module-05/ex00/Bureaucrat.cpp
+++ Module-05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:37:59 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 17:06:36 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 17:07:04 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,7 @@ std::ostream& operator<<(std::ostream& file, const Bureaucrat& bureaucrat)
 	return file;
 }
 
-Bureaucrat::G
+cBureaucrat::GradeTooHighException::what()
 
 {
    	return ("Grade is too high!");
