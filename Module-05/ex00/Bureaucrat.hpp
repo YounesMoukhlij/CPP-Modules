feca218c4cf81b0b28a9360d4b6af200ef4bc67a
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-01 17:04:32 +0100

    by gacp

--- Module-05/ex00/Bureaucrat.hpp
+++ Module-05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 17:04:05 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 17:04:13 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,18 +30,18 @@ class Bureaucrat
 		{
    			public:
    			    const char* what() const noexcept override
-				// {
-   			    //     return ("Grade is too high!");
-   			    // }
+				{
+   			        return ("Grade is too high!");
+   			    }
    		};
 
    		class GradeTooLowException : public std::exception
 		{
    			public:
    			    const char* what() const noexcept override
-				// {
-   			    //     return ("Grade is too low!");
-   			    // }
+				{
+   			        return ("Grade is too low!");
+   			    }
    		};
 			
 		const std::string&	getName() const;
