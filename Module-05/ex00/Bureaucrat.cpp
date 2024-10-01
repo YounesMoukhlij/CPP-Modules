4846f8e3ed25a07868eab9a7e989feccb8055c1a
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-01 17:06:17 +0100

    by gacp

--- Module-05/ex00/Bureaucrat.cpp
+++ Module-05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:37:59 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 17:01:18 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 17:06:14 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,3 +77,8 @@ std::ostream& operator<<(std::ostream& file, const Bureaucrat& bureaucrat)
 	file << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << ".";
 	return file;
 }
+
+
+ {
+   			    //     return ("Grade is too high!");
+   			    // }
