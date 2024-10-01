df6740941331e6642ff3c141f89e9675caf5bc67
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-01 15:51:02 +0100

    by gacp

--- Module-05/ex00/Bureaucrat.hpp
+++ Module-05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/01 13:25:21 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/01 15:51:01 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ class Bureaucrat
 	
 		class GradeTooHighException : public std::exception
 		{
-		
+			const char 
 		};
 		class GradeTooLowException : public std::exception
 		{
