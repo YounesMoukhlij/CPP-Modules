c8ebb7e45f335d69b23140f0d31783f1c09b895b
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-03 17:39:19 +0100

    by gacp

--- Module-05/ex02/ShrubberyCreationForm.cpp
+++ Module-05/ex02/ShrubberyCreationForm.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:50:56 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/03 17:38:55 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/03 17:39:18 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,21 @@ ShrubberyCreationForm::ShrubberyCreationForm(/* args */)
 
 ShrubberyCreationForm::~ShrubberyCreationForm()
 {
+	
 }
 
 
-ShrubberyCreationForm(const std::string& target)
-	        : AForm("ShrubberyCreationForm", 145, 137), target(target) {}
+ShrubberyCreationForm(const std::string& target) : AForm("ShrubberyCreationForm", 145, 137), target(target)
+{
+	
+}
 			
 
 
 void execute(Bureaucrat const & executor) const
-		{
-	        validateExecution(executor);
-	        std::ofstream outfile(target + "_shrubbery");
-	        outfile << "ASCII Trees\n";
-	        outfile.close();
-	    }
+{
+    validateExecution(executor);
+    std::ofstream outfile(target + "_shrubbery");
+    outfile << "ASCII Trees\n";
+    outfile.close();
+}
