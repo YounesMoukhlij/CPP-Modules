8186006a1e7c170f4b02b82412599edf93709293
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-03 18:29:06 +0100

    by gacp

--- Module-05/ex02/RobotomyRequestForm.cpp
+++ Module-05/ex02/RobotomyRequestForm.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:51:45 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/03 18:28:43 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/03 18:28:57 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,11 +49,11 @@ const std::string& RobotomyRequestForm::getTarget(void) const
 
 void	RobotomyRequestForm::RobotomyRequestFormAction(void) const
 {
-		std::cout << "Drilling noises..." << std::endl;
+	std::cout << "Drilling noises..." << std::endl;
 	if (std::rand() % 2)
-		std::cout << this->target << " has been robotomized successfully 50% of the time .\n";
+		std::cout << this->_target << " has been robotomized successfully 50% of the time .\n";
 	else
-		std::cout << "The robotomy of " << this->target << " failed.\n";
+		std::cout << "The robotomy of " << this->_target << " failed.\n";
 }
 
 void	RobotomyRequestForm::execute(Bureaucrat const & bur) const
