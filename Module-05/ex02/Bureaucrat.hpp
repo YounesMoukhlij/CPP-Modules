4f4d1c5e4723b87c2c019349402122f483968f9b
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-03 18:18:28 +0100

    by gacp

--- Module-05/ex02/Bureaucrat.hpp
+++ Module-05/ex02/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/03 18:17:44 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/03 18:18:18 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ class Bureaucrat
 		void				incrementGrade();
 		void				decrementGrade();
 		void				signForm(AForm& form) const;
-		void				executeForm(const AForm& form);
+		void				executeForm(AForm const & form);
 	
 };
 
