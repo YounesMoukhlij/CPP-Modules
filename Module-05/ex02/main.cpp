44dc990a91269f42224b4a70b038477fdfea7064
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-03 17:47:09 +0100

    by gacp

--- Module-05/ex02/main.cpp
+++ Module-05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:05 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/03 17:42:27 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/03 17:46:11 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int main(int you, char **nes)
 
         std::cout << contract << std::endl;  // Show initial form status
         alice.signForm(contract);           // Try to sign the form
-        std::cout << contract << std::endl;  // Show updated form status
+        std::cout << contract << std::endl;  // Show updated form statusscre
     }
 	catch (std::exception& e)
 	{
