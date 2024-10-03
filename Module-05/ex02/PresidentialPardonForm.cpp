353ead1f6198555f3cec83a172c7a309da55d3dc
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-03 16:53:57 +0100

    by gacp

--- Module-05/ex02/PresidentialPardonForm.cpp
+++ Module-05/ex02/PresidentialPardonForm.cpp
@@ -6,8 +6,16 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:52:00 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/03 16:53:40 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/03 16:53:54 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-PresidentialPardonForm.hpp
+#include "PresidentialPardonForm.hpp"
+
+PresidentialPardonForm::PresidentialPardonForm(/* args */)
+{
+}
+
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+}
--- Module-05/ex02/PresidentialPardonForm.hpp
+++ Module-05/ex02/PresidentialPardonForm.hpp
@@ -6,8 +6,19 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:52:02 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/03 16:52:12 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/03 16:53:52 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+class PresidentialPardonForm
+{
+private:
+	/* data */
+public:
+	PresidentialPardonForm(/* args */);
+	~PresidentialPardonForm();
+};
+
+
