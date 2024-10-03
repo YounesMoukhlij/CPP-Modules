67a4f70ff373193fe807977da281f95a5bf27c6d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-03 18:09:12 +0100

    by gacp

--- Module-01/ex04/sed.hpp
+++ Module-01/ex04/sed.hpp
@@ -6,15 +6,15 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 09:29:19 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/09/24 18:59:55 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/03 18:09:07 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SED_HPP
 # define SED_HPP
 
-#include<iostream>
-#include<fstream>
+# include <iostream>
+# include <fstream>
 
 std::string sed_func(std::string str, const std::string s1, const std::string s2);
 
