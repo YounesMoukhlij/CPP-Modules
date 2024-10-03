9debd80effcc737ca6db924e5345d55efcd279a6
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-03 17:38:06 +0100

    by gacp

--- Module-05/ex02/ShrubberyCreationForm.hpp
+++ Module-05/ex02/ShrubberyCreationForm.hpp
@@ -6,30 +6,28 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:50:54 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/03 17:37:48 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/03 17:38:00 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#includ
+#include "AForm.hpp"
 
 class ShrubberyCreationForm : public AForm
 {
-private:
-	/* data */
-public:
-	ShrubberyCreationForm();
-	~ShrubberyCreationForm();
+	public:
+		ShrubberyCreationForm();
+		~ShrubberyCreationForm();
+		
+		ShrubberyCreationForm(const std::string& target)
+	        : AForm("ShrubberyCreationForm", 145, 137), target(target) {}
 	
-	ShrubberyCreationForm(const std::string& target)
-        : AForm("ShrubberyCreationForm", 145, 137), target(target) {}
-
-    void execute(Bureaucrat const & executor) const {
-        validateExecution(executor);
-        std::ofstream outfile(target + "_shrubbery");
-        outfile << "ASCII Trees\n";
-        outfile.close();
-    }
+	    void execute(Bureaucrat const & executor) const {
+	        validateExecution(executor);
+	        std::ofstream outfile(target + "_shrubbery");
+	        outfile << "ASCII Trees\n";
+	        outfile.close();
+	    }
 };
 
