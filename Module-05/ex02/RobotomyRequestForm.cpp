defbb293436f060ac53112ef260f8e3ddf13a792
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-03 16:53:24 +0100

    by gacp

--- Module-05/ex02/RobotomyRequestForm.cpp
+++ Module-05/ex02/RobotomyRequestForm.cpp
@@ -6,7 +6,16 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:51:45 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/03 16:51:45 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/03 16:53:23 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+RobotomyRequestForm.hpp 
+
+RobotomyRequestForm::RobotomyRequestForm(/* args */)
+{
+}
+
+RobotomyRequestForm::~RobotomyRequestForm()
+{
+}
--- Module-05/ex02/RobotomyRequestForm.hpp
+++ Module-05/ex02/RobotomyRequestForm.hpp
@@ -6,8 +6,18 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:51:42 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/03 16:52:18 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/03 16:53:15 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+class RobotomyRequestForm
+{
+private:
+	/* data */
+public:
+	RobotomyRequestForm(/* args */);
+	~RobotomyRequestForm();
+};
+
