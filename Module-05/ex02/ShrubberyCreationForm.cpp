e7fb21ecb43c1316bd2df877b368a325cff6c4e6
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-03 17:39:59 +0100

    by gacp

--- Module-05/ex02/ShrubberyCreationForm.cpp
+++ Module-05/ex02/ShrubberyCreationForm.cpp
@@ -6,20 +6,20 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:50:56 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/03 17:39:38 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/03 17:39:58 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ShrubberyCreationForm.hpp"
 
-ShrubberyCreationForm::ShrubberyCreationForm(/* args */)
+ShrubberyCreationForm::ShrubberyCreationForm()
 {
-	
+	std::cout << "ShrubberyCreationForm Default Constructor" << std::endl;	
 }
 
 ShrubberyCreationForm::~ShrubberyCreationForm()
 {
-	std::cout << "ShrubberyCreationForm Destructor"<<	
+	std::cout << "ShrubberyCreationForm Destructor" << std::endl;	
 }
 
 
