e66e0177b8e69d7bc99e2a0949666219bb002b4b
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-03 17:58:01 +0100

    by gacp

--- Module-05/ex02/PresidentialPardonForm.cpp
+++ Module-05/ex02/PresidentialPardonForm.cpp
@@ -6,16 +6,50 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:52:00 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/03 16:53:54 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/03 17:57:44 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PresidentialPardonForm.hpp"
 
-PresidentialPardonForm::PresidentialPardonForm(/* args */)
+PresidentialPardonForm::PresidentialPardonForm(void) : AForm("PresidentForm", 25, 5)
 {
+	this->target = "defaultTarget";
 }
 
-PresidentialPardonForm::~PresidentialPardonForm()
+PresidentialPardonForm::PresidentialPardonForm(const std::string _target) : AForm("PresidentForm", 25, 5)
 {
+	this->target = _target;
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& PresidentForm) : AForm(PresidentForm.getName(), PresidentForm.getGradeRequiredtoSign(), PresidentForm.getGradeRequiredtoExcute())
+{
+	*this = PresidentForm;	
+}
+
+PresidentialPardonForm::~PresidentialPardonForm(void)
+{
+	
+}
+
+PresidentialPardonForm&  PresidentialPardonForm::operator=(const PresidentialPardonForm& PresidentForm)
+{
+	if (this != &PresidentForm)
+		this->target = PresidentForm.target;
+	return (*this);
+}
+
+void	PresidentialPardonForm::PresidentialPardonFormAction(void) const
+{
+	std::cout << this->target << " has been pardoned by Zaphod Beeblebrox." << std::endl;
+}
+
+void	PresidentialPardonForm::execute(Bureaucrat const & executor) const
+{
+	PresidentialPardonFormAction();
+}
+
+const std::string&  PresidentialPardonForm::getTarget(void) const
+{
+	return (this->target);
 }
