a69bcce684ea304536108c75de705949d5758c42
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 18:30:30 +0100

    by gacp

--- Module-06/ex00/main.cpp
+++ Module-06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:55:06 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 18:30:12 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 18:30:21 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int main(int you, char **nes)
 {
-	()ne;
+	IGNORE(nes);
 
 	if (you != 2)
 	{
