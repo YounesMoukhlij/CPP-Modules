37dc0286f677295f2e0dd20ee462b4f8a2850a36
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 13:43:21 +0100

    by gacp

--- Module-05/ex02/main.cpp
+++ Module-05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:05 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 13:41:34 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 13:43:20 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int main(int you, char **nes)
 	}
 	try
 	{
-		Bureaucrat bureaucrat("saim", 1);
+		Bureaucrat bureaucrat("oo", 1);
 		RobotomyRequestForm roboto("roboto");
 		
 		bureaucrat.signForm(roboto);
