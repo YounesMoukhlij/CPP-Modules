08eef1e164db1a173a3954bed15cc7142e3d8040
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 14:01:42 +0100

    by gacp

--- Module-05/ex03/Intern.hpp
+++ Module-05/ex03/Intern.hpp
@@ -6,7 +6,16 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:56:52 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/03 16:56:52 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 14:01:42 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+class Intern
+{
+private:
+public:
+	Intern(/* args */);
+	~Intern();
+};
