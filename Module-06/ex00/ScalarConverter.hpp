56612e837d2e3a418055bcb574804d688fd58b4e
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 15:10:17 +0100

    by gacp

--- Module-06/ex00/ScalarConverter.hpp
+++ Module-06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:52:34 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 15:09:58 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 15:10:11 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,16 +34,16 @@ class ScalarConverter
 		~ScalarConverter();
 		ScalarConverter& operator= (const ScalarConverter& origine);
 		
-		int	getType(const std::string &str);
-		int	typeDetectionOperation(const std::string &str);
-		int	AnoramlType(const std::string &str);
+		int		getType(const std::string &str);
+		int		typeDetectionOperation(const std::string &str);
+		int		AnoramlType(const std::string &str);
 		void	charOperation(const std::string& str);
 		void	intOperation(const std::string& str);
 		void	floatOperation(const std::string& str);
 		void	doubleOperation(const std::string& str);
-		void	minusInfAndMinusInffOperation(void);
-		void	plusInfAndPlusInffOperation(void);
-		void	nanAndNanfOperation(void);
+		// void	minusInfAndMinusInffOperation(void);
+		// void	plusInfAndPlusInffOperation(void);
+		// void	nanAndNanfOperation(void);
 		void	stringOperation(void);
-		void convert(const std::string& );
+		void 	convert(const std::string& );
 };
