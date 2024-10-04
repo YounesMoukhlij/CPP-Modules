010f399377c3bc5d96b8f9fe89789cb5a8391fee
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 18:33:24 +0100

    by gacp

--- Module-06/ex01/main.cpp
+++ Module-06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:55:06 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 18:33:08 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 18:33:20 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,15 @@ int main(int you, char **nes)
 	try
 	{
 		if (deserializedData == &data)
-	{
-		std::cout << "Operation Done Successfully :)" << std::endl;
-		std::cout << "intValue: " << deserializedData->_intValue << std::endl;
-		std::cout << "charValue: " << deserializedData->_charValue << std::endl;
-		std::cout << "boolValue: " << deserializedData->_boolValue << std::endl;
-		std::cout << "floatValue: " << deserializedData->_floatValue << std::endl;
+		{
+			std::cout << "Operation Done Successfully :)" << std::endl;
+			std::cout << "intValue: " << deserializedData->_intValue << std::endl;
+			std::cout << "charValue: " << deserializedData->_charValue << std::endl;
+			std::cout << "boolValue: " << deserializedData->_boolValue << std::endl;
+			std::cout << "floatValue: " << deserializedData->_floatValue << std::endl;
+		}
 	}
+	catch ()
 	else
 		std::cout << "Operation failed!" << std::endl;
 	return (EXIT_SUCCESS);
