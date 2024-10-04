f9e277faa20f5b92aca195243ff5c236a1008e2c
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 15:06:27 +0100

    by gacp

--- Module-06/ex00/ScalarConverter.cpp
+++ Module-06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:52:37 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 15:06:06 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 15:06:21 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,13 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& _scalar)
 	return (*this);
 }
 
-int	ScalarConverter::T(const std::string &str)
+int	ScalarConverter::typeDetectionOperation(const std::string &str)
 {
 	int i;
 
 	i = 0x0;
 	if (str.size() == 0 || (str.size() == 1 && str[1] == '\0'))
-		return (_CHAR);
+		return (0x0);
 	if (str[i] == '-' || str[i] == '+')
 		i++;
 	if (str[i] == '-' || str[i] == '+')
