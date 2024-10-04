4c9e6de00737d0d714146e857c574b406e2f3211
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 13:44:48 +0100

    by gacp

--- Module-05/ex02/main.cpp
+++ Module-05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:05 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 13:44:31 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 13:44:38 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # include "AForm.hpp"
 # include "RobotomyRequestForm.hpp"
 # include "ShrubberyCreationForm.hpp"
+# include "PresidentialPardonForm.hpp"
 
 int main(int you, char **nes)
 {
