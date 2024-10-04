1a6efb6e3157089c3419e64bc8c106e4d9557ce3
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 18:57:31 +0100

    by gacp

--- Module-06/ex02/Tools.cpp
+++ Module-06/ex02/Tools.cpp
@@ -6,10 +6,15 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 18:56:44 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 18:57:00 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 18:57:28 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#include "Tools.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
 
 Base*	generate(void)
 {
