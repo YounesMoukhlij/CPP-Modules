40d225ac0f6b039e454c9b8ec4a9d9616a725181
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 18:15:23 +0100

    by gacp

---
+++ Module-06/ex01/Data.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Data.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 18:15:19 by youmoukh          #+#    #+#             */
+/*   Updated: 2024/10/04 18:15:21 by youmoukh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+typedef	struct s_data {
+	int		intValue;
+	float	floatValue;
+	char	charValue;
+	bool	boolValue;
+}	Data;
