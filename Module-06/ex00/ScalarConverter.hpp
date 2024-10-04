30f94249ae0cfe5f57ca516a43ee62a6ef413b0a
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 14:58:22 +0100

    by gacp

--- Module-06/ex00/ScalarConverter.hpp
+++ Module-06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:52:34 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 14:58:04 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 14:58:20 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,9 @@ class ScalarConverter
 {
 	public:
 		ScalarConverter();
-		ScalarConverter(const ScalarConverter& );
+		ScalarConverter(const ScalarConverter& origine);
 		~ScalarConverter();
+		ScalarConverter& operator= (const ScalarConverter& o);
 		
 		static int	getType(const std::string &str);
 		static int	detectType(const std::string &str);
@@ -45,6 +46,5 @@ class ScalarConverter
 		static void	plusInfAndPlusInffCase(void);
 		static void	nanAndNanfCase(void);
 		static void	stringCase(void);
-		ScalarConverter& operator= (const ScalarConverter& );
 		static void convert(const std::string& );
 };
