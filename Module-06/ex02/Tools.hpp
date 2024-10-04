4dd757b23fa6de40c4f34c3a0ee9d48d1d964ccd
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 18:55:11 +0100

    by gacp

--- Module-06/ex02/Tools.hpp
+++ Module-06/ex02/Tools.hpp
@@ -6,13 +6,13 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 18:54:18 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 18:54:53 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 18:55:04 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-
+#include "Base.hpp"
 
 Base*	generate(void);
 void	identify(Base* p);
