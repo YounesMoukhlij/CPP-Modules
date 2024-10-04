c2a03c4510606ab0a14fff4196d359f166b7dfde
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 18:42:48 +0100

    by gacp

--- Module-06/ex01/main.cpp
+++ Module-06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:55:06 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 18:42:30 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 18:42:46 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,11 +42,13 @@ int main(int you, char **nes)
 			std::cout << "boolValue: " << deserializedData->_boolValue << std::endl;
 			std::cout << "floatValue: " << deserializedData->_floatValue << std::endl;
 		}
+		puts("123");
 			Serializer::ExceptionSerializer();
 	}
-	catch (std::exception& e)
+	catch (std::exception& younes)
 	{
-        std::cout << "Caught exception: " << e.what() << std::endl;
+		puts("123");
+        std::cout << "Caught exception: " << younes.what() << std::endl;
 	}
 	return (EXIT_SUCCESS);
 }
