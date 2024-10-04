fdce2f656e2b4963e9a0716119dabd51a075ccfc
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 14:55:49 +0100

    by gacp

--- Module-06/ex00/main.cpp
+++ Module-06/ex00/main.cpp
@@ -6,21 +6,21 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:55:06 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 14:55:31 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 14:55:32 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-int main(int you, char **n)
+int main(int you, char **nes)
 {
-	(void)n;
+	(void)nes;
 
 	if (you != 2)
 	{
 		std::cerr << "Error usage: ./Converter arg" << std::endl;
 		return (1);
 	}
-	ScalarConverter::convert(static_cast<std::string>(n[1]));
+	ScalarConverter::convert(static_cast<std::string>(nes[1]));
 	return (0);
 }
