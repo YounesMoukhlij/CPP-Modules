b1a1477b1e90df32edcccd0c5330101c56fc96d7
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 15:10:00 +0100

    by gacp

--- Module-06/ex00/ScalarConverter.hpp
+++ Module-06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:52:34 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 15:09:16 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 15:09:58 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,16 +34,16 @@ class ScalarConverter
 		~ScalarConverter();
 		ScalarConverter& operator= (const ScalarConverter& origine);
 		
-		static int	getType(const std::string &str);
-		static int	typeDetectionOperation(const std::string &str);
-		static int	AnoramlType(const std::string &str);
-		static void	charOperation(const std::string& str);
-		static void	intOperation(const std::string& str);
-		static void	floatOperation(const std::string& str);
-		static void	doubleOperation(const std::string& str);
-		static void	minusInfAndMinusInffOperation(void);
-		static void	plusInfAndPlusInffOperation(void);
-		static void	nanAndNanfOperation(void);
-		static void	stringOperation(void);
-		static void convert(const std::string& );
+		int	getType(const std::string &str);
+		int	typeDetectionOperation(const std::string &str);
+		int	AnoramlType(const std::string &str);
+		void	charOperation(const std::string& str);
+		void	intOperation(const std::string& str);
+		void	floatOperation(const std::string& str);
+		void	doubleOperation(const std::string& str);
+		void	minusInfAndMinusInffOperation(void);
+		void	plusInfAndPlusInffOperation(void);
+		void	nanAndNanfOperation(void);
+		void	stringOperation(void);
+		void convert(const std::string& );
 };
