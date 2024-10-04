c9353c6088214a4d2a890c988267ed63ec512690
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 18:50:29 +0100

    by gacp

--- Module-06/ex02/B.hpp
+++ Module-06/ex02/B.hpp
@@ -6,13 +6,14 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 18:48:39 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 18:50:14 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 18:50:24 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 
-class C
+class B
 {
 	public:
-		~C(void);
+		~B(void);
 };
