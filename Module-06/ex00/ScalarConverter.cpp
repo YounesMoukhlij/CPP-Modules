424d7ce96eb0ed7215719ce138124c04019bb89d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 16:33:48 +0100

    by gacp

--- Module-06/ex00/ScalarConverter.cpp
+++ Module-06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:52:37 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 16:33:26 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 16:33:45 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ int	ScalarConverter::typeDetectionOperation(const std::string &str)
 	printf("i === [%d]\n", i);
 	printf("size === [%lu]\n", str.size());
 	printf("length === [%lu]\n", str.length());
-	if (str[i] == '\0')
+	if (i == str.length())
 		return (_INT_);
 	if (str[i] == '.')
 	{
