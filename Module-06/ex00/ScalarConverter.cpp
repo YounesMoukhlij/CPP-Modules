cc79940cccb141b61a1b486d2035abda9562593b
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 15:17:50 +0100

    by gacp

--- Module-06/ex00/ScalarConverter.cpp
+++ Module-06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:52:37 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 15:17:16 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 15:17:49 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ int	ScalarConverter::typeDetectionOperation(const std::string &str)
 	if (str[i] == '.')
 	{
 		i++;
-		while (isdigit(str[i]))
+		while (str[i] >= 48 && str[i] <= 57)
 			i++;
 		if (str[i] == 'f' && str[i + 1] == '\0')
 			return (_FLOAT_);
