76c65434f410c429784d98a529e9a5be8380fb90
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 17:34:51 +0100

    by gacp

--- Module-06/ex00/ScalarConverter.cpp
+++ Module-06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:52:37 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 17:21:46 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 17:34:50 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,6 +115,7 @@ void	ScalarConverter::intOperation(const std::string& str)
 	std::stringstream	ssInt(str);
 
 	ssInt >> number;
+	n
 	// if (ssInt.fail())
 	// {
 	// 	std::cout << "the number: " << str << " is out of range int" << std::endl;
