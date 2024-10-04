8714d9d4b0d7ab7c6b7631213c3a747a4a661b0f
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 15:09:28 +0100

    by gacp

--- Module-06/ex00/ScalarConverter.hpp
+++ Module-06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:52:34 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 15:09:10 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 15:09:16 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@
 #include <sstream>
 
 
-#define CHAR 0
-#define INT 1
-#define FLOAT 2
-#define DOUBLE 3
-#define MINUSINF 4
-#define PLUSINF 5
-#define NANF 6
-#define STRIN 7
+# define CHAR 0
+# define INT 1
+# define FLOAT 2
+# define DOUBLE 3
+# define MINUSINF 4
+# define PLUSINF 5
+# define NANF 6
+# define STRING 7
 
 class ScalarConverter
 {
