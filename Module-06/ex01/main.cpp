6848a67e8cce4f5ed4356508eb6e9cd8aedf3e31
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 18:37:44 +0100

    by gacp

--- Module-06/ex01/main.cpp
+++ Module-06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:55:06 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 18:35:48 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 18:37:37 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ int main(int you, char **nes)
 			std::cout << "floatValue: " << deserializedData->_floatValue << std::endl;
 		}
 		else
-			ExceptionSerializer();
+			Serializer::ExceptionSerializer();
 	}
 	catch (std::exception& e)
 	{
