199e71e3ffd7503de913b11157d14942abd72f34
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 18:50:00 +0100

    by gacp

--- Module-06/ex02/C.hpp
+++ Module-06/ex02/C.hpp
@@ -6,7 +6,17 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 18:48:42 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 18:48:42 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 18:49:59 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+class C
+{
+	private:
+		/* data */
+	public:
+		C(/* args */);
+		~C();
+};
