ee52b0dd50f2f7139cdc09aa9f743aa18f4c9bd7
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-04 14:06:38 +0100

    by gacp

--- Module-05/ex03/Intern.cpp
+++ Module-05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:56:55 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 14:06:24 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/04 14:06:37 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,34 +36,34 @@ Intern& Intern::operator=(const Intern& intern)
 
 AForm* Intern::makeForm(const std::string& _name, const std::string& _target)
 {
-	AForm *ptr;
-	std::string Name[] = {"Shrubbery", "Roboto", "Presidential"};
+	AForm *pointer;
+	std::string Names[] = {"Shrubbery", "Roboto", "Presidential"};
 
 	int i = -1;
 	int found = -1;
 	while (++i < 3)
 	{
-		if (classesName[i] == _name)
+		if (Names[i] == _name)
 			found = i;
 	}
 	switch (found)
 	{
 		case 0:
-			ptr = new ShrubberyCreationForm(_target);
+			pointer = new ShrubberyCreationForm(_target);
 			break;
 		case 1:
-			ptr = new RobotomyRequestForm(_target);
+			pointer = new RobotomyRequestForm(_target);
 			break;
 		case 2:
-			ptr = new PresidentialPardonForm(_target);
+			pointer = new PresidentialPardonForm(_target);
 			break;
 		default:
 			throw FormNotExistException;
 	}
-	if (!ptr)
+	if (!pointer)
 		throw FailedAllocateMemoryException;
 	std::cout << "Intern creates " << _target << std::endl;
-	return (ptr);
+	return (pointer);
 }
 
 const char* Intern::FormNotExistException::what() const throw()
