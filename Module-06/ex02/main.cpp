334dc0e0413a3b98960d4e9be6b4bb6e2114127e
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 15:33:04 +0100

    by gacp

--- Module-06/ex02/main.cpp
+++ Module-06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:55:06 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 15:21:23 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 15:33:02 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,6 @@ int main(int you, char **nes)
 		std::cout << "An error happend while allocation." << std::endl;
 		return (EXIT_FAILURE);
 	}
+	identify
 	return (EXIT_SUCCESS);
 }
