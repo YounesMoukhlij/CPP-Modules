798ae078f095ab2b2f74a01128ac94279ff0dfd6
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 17:17:11 +0100

    by gacp

--- Module-07/ex02/Array.hpp
+++ Module-07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:40:35 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 17:16:58 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 17:17:11 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,4 +76,7 @@ Array& operator=(const Array &origine)
 }
 
 template<class T>
-unsigned int getS
+unsigned int getSize( void )
+{
+	e
+}
