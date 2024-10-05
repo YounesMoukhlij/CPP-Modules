dbeca05676475602e353e481310cc3fdd244a9bc
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 15:21:22 +0100

    by gacp

--- Module-06/ex02/main.cpp
+++ Module-06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:55:06 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 15:21:09 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 15:21:18 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int main(int you, char **nes)
 	base = generate();
 	if (!base)
 	{
-		std::cout << "An error happend while allocatio"
+		std::cout << "An error happend while allocation." << std::endl;
 	}
 	return (EXIT_SUCCESS);
 }
