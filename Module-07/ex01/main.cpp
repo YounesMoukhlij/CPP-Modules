dc2fc667e125660e033f21619c0912bc836818e0
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 16:31:51 +0100

    by gacp

--- Module-07/ex01/main.cpp
+++ Module-07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:26:32 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 16:29:30 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 16:31:50 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ int main(void)
 	int _int[5] = { 0, 1, 2, 3, 4 };
 	iter(_int, 5, &afficher); std::cout << std::endl;
 	
-	double doubleR[5] = { 0.00, 1.11, 2.22, 3.33, 4.44 };
-	iter(doubleR, 5, &afficher); std::cout << std::endl;
+	double _double[5] = { 0.00, 1.11, 2.22, 3.33, 4.44 };
+	iter(double, 5, &afficher); std::cout << std::endl;
 
 	bool boolR[2] = { false, true };
 	iter(boolR, 2, &afficher); std::cout << std::endl;
