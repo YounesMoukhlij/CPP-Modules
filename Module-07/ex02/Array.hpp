06047809b07a8f469522a2ad6218f5c63dee98e0
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 16:47:26 +0100

    by gacp

--- Module-07/ex02/Array.hpp
+++ Module-07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:40:35 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 16:47:12 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 16:47:23 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ class Array
 	Array();
 	~Array();
 	Array(unsigned int n);
-	Array(unsigned int n);
+	Array(const Array& origine);
 	Array& operator=(const Array &origine);
 }:
