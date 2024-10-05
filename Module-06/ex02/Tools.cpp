5173bdd548d62a1e940fb27e8c0e72acbdadecf9
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 15:10:48 +0100

    by gacp

--- Module-06/ex02/Tools.cpp
+++ Module-06/ex02/Tools.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 18:56:44 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 15:10:29 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 15:10:47 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 Base*	generate(void)
 {
-	
+	int _random = 
 	switch()
 	{
 		case 0;
