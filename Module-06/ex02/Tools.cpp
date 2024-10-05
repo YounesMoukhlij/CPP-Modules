c12802261124583ad558de1a6b8cf99f14e17bee
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 15:32:07 +0100

    by gacp

--- Module-06/ex02/Tools.cpp
+++ Module-06/ex02/Tools.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 18:56:44 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 15:31:46 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 15:32:04 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,6 +73,10 @@ void	identify(Base& p)
 				std::cout << "The Type is B" << std::endl;
 			}
 			catch (std::bad_cast&)
+			{
+		std::cout << "Unknow type" << std::endl;
+
+			}
 		}
 	}
 
