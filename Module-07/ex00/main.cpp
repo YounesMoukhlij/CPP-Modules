052d00660739f30e7f087d9f513ce4fa72c80ed6
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 16:13:58 +0100

    by gacp

--- Module-07/ex00/main.cpp
+++ Module-07/ex00/main.cpp
@@ -6,36 +6,12 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 15:52:54 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 16:04:17 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 16:13:52 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
-
-<Template> T &min(T &t1, T &t2)
-{
-	if (t2 > t1)
-		return (t1);
-	return (t2);
-}
-
-void swap(<Template>T &t1, <Template>T &t2)
-{
-	T tmp;
-
-    tmp = t1;
-	t1 = t2;
-	t2 = tmp;
-}
-
-
-<Template>T &max(<Template>T &t1, <Template>T &t2)
-{
-	if (t1 > t2)
-		return (t1);
-	return (t2);
-}
 
 int main( void )
 {
