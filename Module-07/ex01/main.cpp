4928d9344852ba99cba509edaa64382a49f753fc
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 16:36:45 +0100

    by gacp

--- Module-07/ex01/main.cpp
+++ Module-07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:26:32 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 16:36:32 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 16:36:42 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,12 @@ int main(int you, char **nes)
 	
 	std::string _string[0x4] = { "Younes", "Moukhlij", "1337", "Khouribga"};
 	iter(_string, 0x1, &afficher); std::cout << std::endl;
-	iter(stringR, 0x2, &afficher); std::cout << std::endl;
-	iter(stringR, 0x0, &afficher); std::cout << std::endl;
-	iter(stringR, 0x4, &afficher); std::cout << std::endl;
+	iter(_string, 0x2, &afficher); std::cout << std::endl;
+	iter(_string, 0x0, &afficher); std::cout << std::endl;
+	iter(_string, 0x4, &afficher); std::cout << std::endl;
 	
-	float floatR[0x5] = { 0.01f, 1.1f, 2.2f, 3.3f, 4.43f };
-	iter(floatR, 0x5, &afficher); std::cout << std::endl;
+	float _float[0x5] = { 0.01f, 1.1f, 2.2f, 3.3f, 4.43f };
+	iter(_float, 0x5, &afficher); std::cout << std::endl;
 
 	int _int[0x8] = { 0x4, 0xA, 0x45, 0xC4, 0xB, 0x0, 0x1, 0x9};
 	iter(_int, 0x5, &afficher); std::cout << std::endl;
