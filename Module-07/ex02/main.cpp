739d982b0b9fb3552d11fa76348c0df57cc868f1
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 18:48:31 +0100

    by gacp

--- Module-07/ex02/main.cpp
+++ Module-07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:42:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 18:48:18 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 18:48:31 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,29 +20,18 @@ int main(int you, char**)
 		return (EXIT_FAILURE);
 	}
 	
-	tr
-	Array<int> arr1;
-    std::cout << "Size of arr1: " << arr1.getSize() << std::endl;
-	
-	Array<int> arr2(5);
-    std::cout << "Size of arr2: " << arr2.getSize() << std::endl;
-	
-	Array<std::string>str(3);
-	for (size_t i = 0x0; i <str.getSize() ; i++)
+	try
 	{
-		std::cout << i << std::endl;
+		{/* code */}
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
 	}
 	
+
 	
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    // srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
+	
     // // SCOPE
     // {
     //     Array<int> tmp = numbers;
