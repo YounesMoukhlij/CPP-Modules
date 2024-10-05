e7128b605757d163e64801840a75957b868ba0e9
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 17:52:25 +0100

    by gacp

--- Module-07/ex01/main.cpp
+++ Module-07/ex01/main.cpp
@@ -6,38 +6,66 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:26:32 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 16:39:36 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 17:52:22 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-int main(int you, char **nes)
+// int main(int you, char **nes)
+// {
+// 	IGNORE(nes);
+	
+// 	if (you != 0x1)
+// 	{
+// 		std::cerr << "The Program Does not take any Arguments." << std::endl;
+// 		return (EXIT_FAILURE);
+// 	}
+	
+	
+// 	std::string _string[0x4] = { "Younes", "Moukhlij", "1337", "Khouribga"};
+// 	iter(_string, 0x1, &afficher); std::cout << std::endl;
+// 	iter(_string, 0x2, &afficher); std::cout << std::endl;
+// 	iter(_string, 0x3, &afficher); std::cout << std::endl;
+// 	iter(_string, 0x4, &afficher); std::cout << std::endl;
+	
+// 	float _float[0x5] = { 0.01f, 1.1f, 2.2f, 3.3f, 4.43f };
+// 	iter(_float, 0x5, &afficher); std::cout << std::endl;
+
+// 	int _int[0x8] = { 0x4, 0xA, 0x45, 0xC4, 0xB, 0x0, 0x1, 0x9};
+// 	iter(_int, 0x5, &afficher); std::cout << std::endl;
+	
+// 	double _double[0x5] = { 0.00, 1.11, 2.22, 3.33, 4.44 };
+// 	iter(_double, 0x5, &afficher); std::cout << std::endl;
+
+
+// 	return (EXIT_SUCCESS);
+// }
+
+class Awesome
 {
-	IGNORE(nes);
-	
-	if (you != 0x1)
-	{
-		std::cerr << "The Program Does not take any Arguments." << std::endl;
-		return (EXIT_FAILURE);
-	}
-	
-	
-	std::string _string[0x4] = { "Younes", "Moukhlij", "1337", "Khouribga"};
-	iter(_string, 0x1, &afficher); std::cout << std::endl;
-	iter(_string, 0x2, &afficher); std::cout << std::endl;
-	iter(_string, 0x3, &afficher); std::cout << std::endl;
-	iter(_string, 0x4, &afficher); std::cout << std::endl;
-	
-	float _float[0x5] = { 0.01f, 1.1f, 2.2f, 3.3f, 4.43f };
-	iter(_float, 0x5, &afficher); std::cout << std::endl;
+  public:
+    Awesome(void) : _n(0) {}
+    Awesome( int n ) : _n( n ) {}
+    Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+    bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+    bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+    bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+    bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+    bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+    bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+    int get_n() const { return _n; }
+  private:
+    int _n;
+};
 
-	int _int[0x8] = { 0x4, 0xA, 0x45, 0xC4, 0xB, 0x0, 0x1, 0x9};
-	iter(_int, 0x5, &afficher); std::cout << std::endl;
-	
-	double _double[0x5] = { 0.00, 1.11, 2.22, 3.33, 4.44 };
-	iter(_double, 0x5, &afficher); std::cout << std::endl;
+int main(void)
+{
+	Awesome a(2), b(4);
 
-
-	return (EXIT_SUCCESS);
+        swap(a, b);
+        std::cout << a << " " << b << std::endl;
+        std::cout << max(a, b) << std::endl;
+        std::cout << min(a, b) << std::endl;
+        return (0);
 }
