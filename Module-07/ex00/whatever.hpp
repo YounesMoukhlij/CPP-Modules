cb2a1345bc3abba85fe88dca86c3932744dc1015
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 16:10:17 +0100

    by gacp

--- Module-07/ex00/whatever.hpp
+++ Module-07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 15:52:12 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 16:03:26 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 16:10:16 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,14 @@
 template<typename T> T &max(T &t1, T &t2);
 
 
-template<typename T>
-void swap(T &t1, T &t2);
+void swap(<Template>T &t1, <Template>T &t2)
+{
+	T tmp;
+
+    tmp = t1;
+	t1 = t2;
+	t2 = tmp;
+}
 
 template<typename T>
 T &min(T &t1, T &t2);
