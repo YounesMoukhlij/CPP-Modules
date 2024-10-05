3e7d7d47c105c376e9d448c4d7cfe223f05988c8
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 16:33:31 +0100

    by gacp

--- Module-07/ex01/main.cpp
+++ Module-07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:26:32 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 16:33:12 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 16:33:23 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ int main(void)
 	int _int[0x8] = { 0x4, 0xA, 0x45, 0xC4, 0xB, 0x0, 0x1, 0x9};
 	iter(_int, 0x5, &afficher); std::cout << std::endl;
 	
-	double _double[5] = { 0.00, 1.11, 2.22, 3.33, 4.44 };
-	iter(_double, 5, &afficher); std::cout << std::endl;
+	double _double[0x5] = { 0.00, 1.11, 2.22, 3.33, 4.44 };
+	iter(_double, 0x5, &afficher); std::cout << std::endl;
 
 
 	return (0);
