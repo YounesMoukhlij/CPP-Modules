f381de3cf11c453be353923c89137438fda03f97
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 17:30:13 +0100

    by gacp

--- Module-07/ex02/Array.hpp
+++ Module-07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:40:35 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 17:29:59 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 17:30:10 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,7 @@ unsigned int Array<T>::getSize( void ) const
 	return (_size);
 }
 
-  const T& Array<T>::operator[](unsigned int index) const {
+  const Array<T>& Array<T>::operator[](unsigned int index) const {
         if (index >= _size) {
             throw std::out_of_range("Index out of bounds");
         }
@@ -93,7 +93,7 @@ unsigned int Array<T>::getSize( void ) const
     }
 
     // Subscript operator (non-const version for write access)
-    T& operator[](unsigned int index) {
+    T& Array<T>::operator[](unsigned int index) {
         if (index >= _size) {
             throw std::out_of_range("Index out of bounds");
         }
