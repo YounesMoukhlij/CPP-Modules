bbd9587eaac963439690158c82bbddd2021ae20d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 13:40:19 +0100

    by gacp

--- Module-06/ex01/main.cpp
+++ Module-06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:55:06 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 13:39:42 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 13:40:19 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 class younes
 {
 	public:
-		~younes();
+		~younes()
 		younes();
 		void print()
 		{
