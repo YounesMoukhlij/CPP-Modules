b5e4890f5ba31a736ec0a463d15693a7510481f9
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 12:28:04 +0100

    by gacp

--- Module-06/ex02/Tools.cpp
+++ Module-06/ex02/Tools.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 18:56:44 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 18:57:31 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 12:28:03 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,14 @@ void	identify(Base* p)
 
 }
 
+
+
+
+
 void	identify(Base& p)
 {
 
 }
+
+
+
