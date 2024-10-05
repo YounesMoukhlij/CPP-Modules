e31863d7353be6e13547cee56c6eab7f091a4536
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 15:44:22 +0100

    by gacp

--- Module-06/ex02/Tools.cpp
+++ Module-06/ex02/Tools.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 18:56:44 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 15:44:08 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 15:44:15 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,11 +38,11 @@ Base*	generate(void)
 void	identify(Base* p)
 {
 	if (dynamic_cast<A*>(p))
-		std::cout << "By Reference The Type is : A" << std::endl;
+		std::cout << "By Reference The Type is : [A]" << std::endl;
 	else if (dynamic_cast<B*>(p))
-		std::cout << "By Reference The Type is : B" << std::endl;
+		std::cout << "By Reference The Type is : [B]" << std::endl;
 	else if (dynamic_cast<C*>(p))
-		std::cout << "By Reference The Type is : C" << std::endl;
+		std::cout << "By Reference The Type is : [C]" << std::endl;
 	else
 		std::cout << "Unknow type" << std::endl;
 }
@@ -68,7 +68,7 @@ void	identify(Base& p)
 			try 
 			{
 				(void)dynamic_cast<C&>(p);
-				std::cout << "By pointer The Type is : C" << std::endl;
+				std::cout << "By pointer The Type is : [C]" << std::endl;
 			}
 			catch (std::bad_cast&)
 			{
