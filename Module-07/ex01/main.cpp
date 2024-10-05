ff630bff4f9a5d7a293c053dc8c136e4d7bd4590
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 16:32:16 +0100

    by gacp

--- Module-07/ex01/main.cpp
+++ Module-07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:26:32 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 16:32:03 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 16:32:15 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ int main(void)
 {
 
 	std::string stringR[0x4] = { "Younes", "Moukhlij", "1337", "Khouribga"};
-	iter(stringR, 5, &afficher); std::cout << std::endl;
-	iter(stringR, 2, &afficher); std::cout << std::endl;
-	iter(stringR, 0, &afficher); std::cout << std::endl;
+	iter(stringR, 0x1, &afficher); std::cout << std::endl;
+	iter(stringR, 0x2, &afficher); std::cout << std::endl;
+	iter(stringR, 0x0, &afficher); std::cout << std::endl;
+	iter(stringR, 0x4, &afficher); std::cout << std::endl;
+	
 	float floatR[5] = { 0.0f, 1.1f, 2.2f, 3.3f, 4.4f };
 	iter(floatR, 5, &afficher); std::cout << std::endl;
 
