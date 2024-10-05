1ba5764aabdca0e3e371c1c81dacdf3443c3a191
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 13:38:06 +0100

    by gacp

--- Module-06/ex01/main.cpp
+++ Module-06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:55:06 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 13:37:52 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 13:38:06 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 class younes
 {
-	
+	private :
+		younes();
+	public:
 }
 
 
