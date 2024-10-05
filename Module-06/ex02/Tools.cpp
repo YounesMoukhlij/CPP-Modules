86b12ae608ea5766af7dbc27a08921e94baeff0e
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 15:28:45 +0100

    by gacp

--- Module-06/ex02/Tools.cpp
+++ Module-06/ex02/Tools.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 18:56:44 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 15:28:31 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 15:28:44 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,9 @@ void	identify(Base* p)
 void	identify(Base& p)
 {
 	try
+	{
+		dynamic_cast<A&>()
+	}
 
 }
 
