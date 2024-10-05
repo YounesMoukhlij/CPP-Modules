099d79c1aa5962c1accbcb65f276a8f37246e5f0
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 17:59:24 +0100

    by gacp

--- Module-07/ex01/main.cpp
+++ Module-07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:26:32 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 17:59:05 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 17:59:22 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,11 +69,9 @@ int main(int you, char **nes)
 	
 
 	int tab[] = { 0, 1, 2, 3, 4 };
-	 Awesome tab2[5];
+	Awesome tab2[5];
 
-  iter( tab, 5, print<const int> );
-
-
+  	iter( tab, 5, print<const int> );
 	return (EXIT_SUCCESS);
 }
 
