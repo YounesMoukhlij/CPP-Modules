4f54f3c73215d32230c3ed0acd03091e5094004a
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 15:14:45 +0100

    by gacp

--- Module-06/ex02/Tools.cpp
+++ Module-06/ex02/Tools.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 18:56:44 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 15:14:32 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 15:14:34 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ Base*	generate(void)
 			return (new A());
 		case 0x2;
 			return (new B());
-		case 3;
+		case 0x3;
 			return (new C());
 		default:
 			return (0x0);
