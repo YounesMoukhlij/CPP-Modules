61e10110967b081d3ad87aba73f12f72404f0dfd
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 15:15:33 +0100

    by gacp

--- Module-06/ex02/Base.hpp
+++ Module-06/ex02/Base.hpp
@@ -6,13 +6,14 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 18:47:53 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 18:47:54 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 15:15:31 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <iostream>
+# include <iostream>
+# include <cstdlib>
 
 class Base
 {
