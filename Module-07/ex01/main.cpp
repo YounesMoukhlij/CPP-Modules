680f6890a79b3b0dbe9228d0eafbcf40b6cbba51
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 16:26:34 +0100

    by gacp

--- Module-07/ex01/main.cpp
+++ Module-07/ex01/main.cpp
@@ -1,24 +1,15 @@
-#include <iostream>
-#include <cctype>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/05 16:26:32 by youmoukh          #+#    #+#             */
+/*   Updated: 2024/10/05 16:26:32 by youmoukh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-template<typename T>
-void display(T const &v)
-{
-	std::cout << v << " ";
-}
-
-
-
-
-
-
-
-template<typename T>
-void iter(T *arr, size_t length, void (*f)(T const &v))
-{
-	for (size_t i = 0; i < length; i++)
-		(*f)(arr[i]);
-}
 
 
 int main(void)
