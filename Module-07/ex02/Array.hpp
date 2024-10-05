10ed513e25f1bba87f8c3031fa0498753c5aff11
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 17:32:47 +0100

    by gacp

--- Module-07/ex02/Array.hpp
+++ Module-07/ex02/Array.hpp
@@ -6,13 +6,14 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:40:35 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 17:31:16 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 17:32:43 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 # include <iostream>
+# include <exception>
 
 # define MAX_VAL 750
 # define IGNORE(param) (void)(param)
