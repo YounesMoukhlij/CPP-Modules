4da37b9df8ee7f25dc11751a572bdc6a71d7604d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 16:41:55 +0100

    by gacp

--- Module-07/ex02/Array.hpp
+++ Module-07/ex02/Array.hpp
@@ -6,11 +6,11 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:40:35 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 16:41:42 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 16:41:43 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma on
+#pragma once
 
 # include <iostream>
 
