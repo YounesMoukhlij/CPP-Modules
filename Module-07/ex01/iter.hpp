b3d061516e00d08f1527948209dfbab7c9a61aca
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 16:30:09 +0100

    by gacp

--- Module-07/ex01/iter.hpp
+++ Module-07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:25:23 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 16:29:49 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 16:30:05 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@
 
 template<typename T> void afficher(T const &_value)
 {
-	std::cout << v << " ";
+	std::cout << _value << " ";
 }
 
-template<typename T> void iter(T *arr, size_t length, void (*f)(T const &_value))
+template<typename T> void iter(T *array, size_t length, void (*f)(T const &_value))
 {
 	for (size_t i = 0; i < length; i++)
-		(*f)(arr[i]);
+		(*f)(array[i]);
 }
