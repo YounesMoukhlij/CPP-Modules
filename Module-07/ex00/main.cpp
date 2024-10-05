38ae2027d39b4bd1e1674761b9c13b3162d473c5
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 17:55:56 +0100

    by gacp

--- Module-07/ex00/main.cpp
+++ Module-07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 15:52:54 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 17:55:40 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 17:55:54 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,40 +30,38 @@ class Awesome
 };
 
 
-// int main(int you, char **nes)
-// {
-// 	IGNORE(nes);
-	
-// 	if (you != 0x1)
-// 	{
-// 		std::cerr << "The Program Does not take any Arguments." << std::endl;
-// 		return (EXIT_FAILURE);
-// 	}
-//     int a = 0x2;
-//     int b = 0x3;
-//     swap( a, b );
-//     std::cout << "a = " << a << ", b = " << b << std::endl;
-//     std::cout << "min( a, b ) = " << min( a, b ) << std::endl;
-//     std::cout << "max( a, b ) = " << max( a, b ) << std::endl;
-//     std::string c = "chaine1";
-//     std::string d = "chaine2";
-//     std::cout << "Before Swap" << std::endl;
-//     std::cout << "c = " << c << ", d = " << d << std::endl;
-//     swap(c, d);
-//     std::cout << "After Swap" << std::endl;
-//     std::cout << "c = " << c << ", d = " << d << std::endl;
-//     std::cout << "min( c, d ) = " << min( c, d ) << std::endl;
-//     std::cout << "max( c, d ) = " << max( c, d ) << std::endl;
-	
-//     return (EXIT_SUCCESS);
-// }
-
-
-
-std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
-
-int main(void)
+int main(int you, char **nes)
 {
+	IGNORE(nes);
 	
-        return (0);
+	if (you != 0x1)
+	{
+		std::cerr << "The Program Does not take any Arguments." << std::endl;
+		return (EXIT_FAILURE);
+	}
+    int a = 0x2;
+    int b = 0x3;
+    swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    std::cout << "Before Swap" << std::endl;
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    swap(c, d);
+    std::cout << "After Swap" << std::endl;
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << max( c, d ) << std::endl;
+		
+		
+		Awesome a(2), b(4);
+
+        swap(a, b);
+        std::cout << a << " " << b << std::endl;
+        std::cout << max(a, b) << std::endl;
+        std::cout << min(a, b) << std::endl;
+		
+    return (EXIT_SUCCESS);
 }
