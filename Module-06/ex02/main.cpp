10e267ef160549b96d96f27d6836ef6bbd9d906d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 15:17:33 +0100

    by gacp

--- Module-06/ex02/main.cpp
+++ Module-06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:55:06 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 15:17:15 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 15:17:31 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ int main(int you, char **nes)
 		std::cerr << "The Program Does not take any Arguments." << std::endl;
 		return (EXIT_FAILURE);
 	}
+	Base *base;
+
 	
 	return (EXIT_SUCCESS);
 }
