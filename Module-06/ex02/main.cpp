0e77f90aacc9e6dc034726207b9c78626b270d5b
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 15:44:53 +0100

    by gacp

--- Module-06/ex02/main.cpp
+++ Module-06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:55:06 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 15:44:33 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 15:44:50 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,15 @@
 #include "C.hpp"
 #include "Tools.hpp"
 
+void younes()
+{
+	system("leaks Dyc")
+}
+
 int main(int you, char **nes)
 {
 	IGNORE(nes);
-	atexit
+	atexit(younes);
 	if (you != 1)
 	{
 		std::cerr << "The Program Does not take any Arguments." << std::endl;
