9e8e5408f825b0d1205d3efb26e99cfe26b9e518
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 17:38:49 +0100

    by gacp

--- Module-07/ex02/main.cpp
+++ Module-07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:42:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 17:38:36 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 17:38:41 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ int main(int you, char**)
     std::cout << "Size of arr1: " << arr1.getSize() << std::endl;
 	
 	
+	Array<int> arr2(5);
+    std::cout << "Size of arr2: " << arr2.getSize() << std::endl;
     // Array<int> numbers(MAX_VAL);
     // int* mirror = new int[MAX_VAL];
     // srand(time(NULL));
