cd8c7a956b1edd1ac3a606bfba85ad8e4418dd92
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 16:17:15 +0100

    by gacp

--- Module-07/ex00/main.cpp
+++ Module-07/ex00/main.cpp
@@ -6,15 +6,22 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 15:52:54 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 16:15:03 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 16:17:12 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
 
-int main( void )
+int main(int you, char **nes)
 {
+	IGNORE(nes);
+	
+	if (you != 1)
+	{
+		std::cerr << "The Program Does not take any Arguments." << std::endl;
+		return (EXIT_FAILURE);
+	}
     int a = 2;
     int b = 3;
     swap( a, b );
