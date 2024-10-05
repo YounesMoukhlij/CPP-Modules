fe6b52a7fe60745c94c4249f27f3aa56046ed94b
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 15:26:09 +0100

    by gacp

--- Module-06/ex02/Tools.cpp
+++ Module-06/ex02/Tools.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 18:56:44 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 15:24:06 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 15:26:09 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ Base*	generate(void)
 
 void	identify(Base* p)
 {
-	(void) p;
+	if ()
 
 }
 
