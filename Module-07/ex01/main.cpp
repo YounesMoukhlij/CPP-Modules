7740eb060cfed8343e70140334645eee7974df90
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 16:32:04 +0100

    by gacp

--- Module-07/ex01/main.cpp
+++ Module-07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:26:32 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 16:31:50 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 16:32:03 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 int main(void)
 {
 
+	std::string stringR[0x4] = { "Younes", "Moukhlij", "1337", "Khouribga"};
+	iter(stringR, 5, &afficher); std::cout << std::endl;
+	iter(stringR, 2, &afficher); std::cout << std::endl;
+	iter(stringR, 0, &afficher); std::cout << std::endl;
 	float floatR[5] = { 0.0f, 1.1f, 2.2f, 3.3f, 4.4f };
 	iter(floatR, 5, &afficher); std::cout << std::endl;
 
@@ -22,15 +26,8 @@ int main(void)
 	iter(_int, 5, &afficher); std::cout << std::endl;
 	
 	double _double[5] = { 0.00, 1.11, 2.22, 3.33, 4.44 };
-	iter(double, 5, &afficher); std::cout << std::endl;
+	iter(_double, 5, &afficher); std::cout << std::endl;
 
-	bool boolR[2] = { false, true };
-	iter(boolR, 2, &afficher); std::cout << std::endl;
-
-	std::string stringR[0x4] = { "Younes", "Moukhlij", "1337", "Khouribga"};
-	iter(stringR, 5, &afficher); std::cout << std::endl;
-	iter(stringR, 2, &afficher); std::cout << std::endl;
-	iter(stringR, 0, &afficher); std::cout << std::endl;
 
 	return (0);
 }
