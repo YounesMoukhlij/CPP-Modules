067f032924c61566841f5f492587e543f3208614
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 16:33:48 +0100

    by gacp

--- Module-07/ex01/main.cpp
+++ Module-07/ex01/main.cpp
@@ -6,15 +6,21 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:26:32 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 16:33:23 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 16:33:46 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-int main(void)
+int main(int you, char **nes)
 {
-
+	IGNORE(nes);
+	
+	if (you != 0x1)
+	{
+		std::cerr << "The Program Does not take any Arguments." << std::endl;
+		return (EXIT_FAILURE);
+	}
 	std::string stringR[0x4] = { "Younes", "Moukhlij", "1337", "Khouribga"};
 	iter(stringR, 0x1, &afficher); std::cout << std::endl;
 	iter(stringR, 0x2, &afficher); std::cout << std::endl;
