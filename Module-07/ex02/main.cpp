297865fbb7fbdd9f87c6d94d97907a754cbeb2a2
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 18:46:11 +0100

    by gacp

--- Module-07/ex02/main.cpp
+++ Module-07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:42:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 18:45:58 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 18:46:11 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ int main(int you, char**)
     // srand(time(NULL));
     for (int i = 0; i < MAX_VAL; i++)
     {
-        const int value = rand();
+         int value = rand();
         numbers[i] = value;
         mirror[i] = value;
     }
