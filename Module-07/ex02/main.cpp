6856cd1e626cb364fd2a219e2ff5c198f36edfaa
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 18:43:31 +0100

    by gacp

--- Module-07/ex02/main.cpp
+++ Module-07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:42:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 18:43:18 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 18:43:31 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,10 @@ int main(int you, char**)
     std::cout << "Size of arr2: " << arr2.getSize() << std::endl;
 	
 	Array<std::string>str(3);
-	for (int i = 0x0;i <str.getSize();i+)
+	for (int i = 0x0;i <str.getSize();i++)
+	{
+		std::cout << str[i] << std
+	}
 	
 	
     // Array<int> numbers(MAX_VAL);
