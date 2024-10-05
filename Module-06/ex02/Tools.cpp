6cfdb041c638dcbfe9bd141a910c230bea73326a
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 15:16:03 +0100

    by gacp

--- Module-06/ex02/Tools.cpp
+++ Module-06/ex02/Tools.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 18:56:44 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 15:14:48 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 15:16:01 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,16 @@ Base*	generate(void)
 	
 	switch(_random)
 	{
-		case 0x0;
+		case 0x0:
 			return (new A());
-		case 0x2;
+		case 0x2:
 			return (new B());
-		case 0x3;
+		case 0x3:
 			return (new C());
 		default:
 			return (0x0);
 	}
 }
-
-
 
 
 void	identify(Base* p)
