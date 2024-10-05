576d0d9b09403deb4b7afb95cd2d58ac9c7154e3
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 18:52:18 +0100

    by gacp

--- Module-07/ex02/main.cpp
+++ Module-07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:42:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 18:51:57 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 18:52:09 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ int main(int you, char**)
 		// }
 		
 		Array<int> arr3(arr1);
-    	std::cout << "Size of arr2: " << arr3.getSize() << std::endl;
+    	std::cout << "Size of arr3: " << arr3.getSize() << std::endl;
 	}
 	catch(const std::exception& e)
 	{
