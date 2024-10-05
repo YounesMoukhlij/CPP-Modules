1f456674493f4c26fb06b3dffd6c5f3dfcc1ee06
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 17:59:40 +0100

    by gacp

--- Module-07/ex01/main.cpp
+++ Module-07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:26:32 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 17:59:22 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 17:59:34 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,11 @@ int main(int you, char **nes)
 	double _double[0x5] = { 0.00, 1.11, 2.22, 3.33, 4.44 };
 	iter(_double, 0x5, &afficher); std::cout << std::endl;
 	
+	std::cout << std::endl;
+	std::cout << "Another Test."<< std::endl;
+	std::cout << std::endl;
 
+	
 	int tab[] = { 0, 1, 2, 3, 4 };
 	Awesome tab2[5];
 
