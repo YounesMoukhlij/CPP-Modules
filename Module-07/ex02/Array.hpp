5a3ba84238b8be42a47264bea959bc106c89a937
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-05 17:18:12 +0100

    by gacp

--- Module-07/ex02/Array.hpp
+++ Module-07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:40:35 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 17:17:58 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/05 17:18:07 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,10 +47,8 @@ Array<T>::Array() : _array(0x0), _size(0x0)
 }
 
 template<class T>
-Array<T>::Array(unsigned int n) : _size(n), _array(new T[_])
+Array<T>::Array(unsigned int n) : _size(n), _array(new T[n])
 {
-	_size = n;
-	new T[_size];
 }
 
 template<class T>
