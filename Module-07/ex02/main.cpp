799d30fe7eee341d8df5b3b536ab9577447183ee
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-06 14:44:06 +0100

    commit with gacp

--- Module-07/ex02/main.cpp
+++ Module-07/ex02/main.cpp
@@ -17,45 +17,95 @@ void	younes()
 	system("leaks iter");
 }
 
-int main(int you, char**)
+// int main(int you, char**)
+// {
+// 	// atexit(younes);
+// 	if (you != 0x1)
+// 	{
+// 		std::cerr << "The Program Does not take any Arguments." << std::endl;
+// 		return (EXIT_FAILURE);
+// 	}
+
+// 	try
+// 	{
+// 		Array<int> arr1;
+//     	std::cout << "Size of arr1: " << arr1.getSize() << std::endl;
+
+// 		Array<int> arr2(5);
+//     	std::cout << "Size of arr2: " << arr2.getSize() << std::endl;
+
+
+// 		Array<std::string>str(3);
+// 		for (size_t i = 0x0; i < str.getSize() ; i++)
+// 		{
+// 			std::cout << i << std::endl;
+// 		}
+
+// 		Array<int> arr3(arr1);
+//     	std::cout << "Size of arr3: " << arr3.getSize() << std::endl;
+// 		Array<int> numbers(MAX_VAL);
+//     	int* mirror = new int[MAX_VAL];
+// 		for (unsigned int i = 0; i < MAX_VAL; i++)
+//     	{
+//         	const unsigned int value = rand();
+//         	numbers[i] = value;
+//         	mirror[i] = value;
+//     	}
+// 	}
+// 	catch(const std::exception& e)
+// 	{
+// 		std::cerr << e.what() << '\n';
+// 	}
+
+//     return 0;
+// }
+
+int main(int, char**)
 {
-	// atexit(younes);
-	if (you != 0x1)
-	{
-		std::cerr << "The Program Does not take any Arguments." << std::endl;
-		return (EXIT_FAILURE);
-	}
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
 
-	try
-	{
-		Array<int> arr1;
-    	std::cout << "Size of arr1: " << arr1.getSize() << std::endl;
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-		Array<int> arr2(5);
-    	std::cout << "Size of arr2: " << arr2.getSize() << std::endl;
-
-
-		Array<std::string>str(3);
-		for (size_t i = 0x0; i < str.getSize() ; i++)
-		{
-			std::cout << i << std::endl;
-		}
-
-		Array<int> arr3(arr1);
-    	std::cout << "Size of arr3: " << arr3.getSize() << std::endl;
-		Array<int> numbers(MAX_VAL);
-    	int* mirror = new int[MAX_VAL];
-		for (unsigned int i = 0; i < MAX_VAL; i++)
-    	{
-        	const unsigned int value = rand();
-        	numbers[i] = value;
-        	mirror[i] = value;
-    	}
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
     return 0;
 }
