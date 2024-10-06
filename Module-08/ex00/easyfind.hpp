e1e1d7679ee54e8d1634901b684fd6d80750e4ca
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-06 16:01:31 +0100

    commit with gacp

--- Module-08/ex00/easyfind.hpp
+++ Module-08/ex00/easyfind.hpp
@@ -2,7 +2,7 @@
 
 
 template <typename T>
-typename T::iterator easyfind(T& container, int _value)
+typename T::iterator easyfind(T& container, int _integer)
 {
 	typename T::iterator i = std::find(container.begin(), container.end(), value);
 	return i;
