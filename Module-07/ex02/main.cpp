14e664c945e44a7d9c8e694e049fa71f8309363b
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-06 14:57:12 +0100

    commit with gacp

--- Module-07/ex02/main.cpp
+++ Module-07/ex02/main.cpp
@@ -109,6 +109,8 @@ int main(int you, char **nes)
 	    {
     	    std::cerr << e.what() << '\n';
     	}
+		for (size_t i = 0x0; i < arr4.getSize(); i++)
+			std::cout << "arr4[" << i << "] = " << arr4[i] << std::endl;
 
 
 
