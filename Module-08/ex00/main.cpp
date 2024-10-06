3b567abed6a7fb98d82b8e85a0531ae08b58ce96
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-06 16:03:02 +0100

    commit with gacp

--- Module-08/ex00/main.cpp
+++ Module-08/ex00/main.cpp
@@ -5,5 +5,12 @@
 
 int main(int you, char **nes)
 {
-    
+	atexit(younes);
+	IGNORE(nes);
+	if (you != 0x1)
+	{
+		std::cerr << "The Program Does not take any Arguments." << std::endl;
+		return (EXIT_FAILURE);
+	}
+
 }
