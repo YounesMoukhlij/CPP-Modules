94f02bf3bb14d653e1f0094524c463a5acb42c5c
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-06 16:17:23 +0100

    commit with gacp

--- Module-08/ex01/Span.hpp
+++ Module-08/ex01/Span.hpp
@@ -5,3 +5,22 @@
 
 
 # define IGNORE(param) (void)(param)
+
+class Span {
+
+	private:
+		int*			_arrayElements;
+		std::size_t	_size;
+		std::size_t	_index;
+	public:
+		Span();
+		Span(unsigned int _N);
+		Span(const Span& _obj);
+		~Span();
+
+		Span& operator=(const Span& _obj);
+		void	addNumber(int _number);
+		int		shortestSpan();
+		int		longestSpan();
+		void	FillArray();
+};
