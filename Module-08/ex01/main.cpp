bf64c3c66740a2b560ddcaa810dc76ecd668a191
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-06 16:16:17 +0100

    commit with gacp

---
+++ Module-08/ex01/main.cpp
@@ -0,0 +1,41 @@
+
+
+
+#include "Span.hpp"
+
+int main(int you, char **nes)
+{
+	IGNORE(nes);
+	if (you != 0x1)
+	{
+		std::cerr << "The Program Does not take any Arguments." << std::endl;
+		return (EXIT_FAILURE);
+	}
+
+    std::vector<int> vect;
+
+    vect.push_back(0x0);
+    vect.push_back(0xA);
+    vect.push_back(0xB);
+    vect.push_back(0xC);
+    vect.push_back(0xE);
+    vect.push_back(0xF);
+
+    for (std::vector<int>::iterator i = vect.begin(); i < vect.end(); i++)
+    {
+        std::cout << "Element of vector<int> = " << *i << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+    try
+    {
+        std::vector<int>::iterator  isthere = easyfind(vect, 100);
+        std::cout << "Element has been found = [" << *isthere << "]" << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+	return (EXIT_SUCCESS);
+}
