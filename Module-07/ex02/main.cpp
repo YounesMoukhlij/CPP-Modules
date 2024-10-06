c093b8af936f90e7f8b4579b78e1dfc82c255634
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-06 14:57:35 +0100

    commit with gacp

--- Module-07/ex02/main.cpp
+++ Module-07/ex02/main.cpp
@@ -109,7 +109,7 @@ int main(int you, char **nes)
 	    {
     	    std::cerr << e.what() << '\n';
     	}
-		std::s
+		std::cout << "Check after modifing the value." << std::endl;
 		for (size_t i = 0x0; i < arr4.getSize(); i++)
 			std::cout << "arr4[" << i << "] = " << arr4[i] << std::endl;
 
