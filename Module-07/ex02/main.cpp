1f8cefc4e8293d815ce81757eaedce7a6e0ad592
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-06 14:30:44 +0100

    commit with gacp

--- Module-07/ex02/main.cpp
+++ Module-07/ex02/main.cpp
@@ -18,36 +18,36 @@ void	younes()
 }
 
 int main(int you, char**)
-{	
+{
 	// atexit(younes);
 	if (you != 0x1)
 	{
 		std::cerr << "The Program Does not take any Arguments." << std::endl;
 		return (EXIT_FAILURE);
 	}
-	
+
 	try
 	{
 		Array<int> arr1;
     	std::cout << "Size of arr1: " << arr1.getSize() << std::endl;
-		
+
 		Array<int> arr2(5);
     	std::cout << "Size of arr2: " << arr2.getSize() << std::endl;
-		
-		
+
+
 		Array<std::string>str(3);
 		for (size_t i = 0x0; i < str.getSize() ; i++)
 		{
 			std::cout << i << std::endl;
 		}
-		
+
 		Array<int> arr3(arr1);
     	std::cout << "Size of arr3: " << arr3.getSize() << std::endl;
 		Array<int> numbers(MAX_VAL);
     	int* mirror = new int[MAX_VAL];
-		for (int i = 0; i < MAX_VAL; i++)
+		for (uni i = 0; i < MAX_VAL; i++)
     	{
-        	 int value = rand();
+        	unsigned int value = rand();
         	numbers[i] = value;
         	mirror[i] = value;
     	}
@@ -56,6 +56,6 @@ int main(int you, char**)
 	{
 		std::cerr << e.what() << '\n';
 	}
-	
+
     return 0;
 }
