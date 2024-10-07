62ee77b898d641bcf2684bea5f71c89ad8aae0f2
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 13:40:56 +0100

    by gacp

--- Module-08/ex01/Span.cpp
+++ Module-08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 13:40:29 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 13:40:32 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,7 +99,7 @@ void	Span::addNumber(usd n)
 		_i++;
 	}
 	else
-		throw std::out ("Cannot add the number, Out of range.");
+		throw std::out_of_range("Cannot add the number, Out of range.");
 }
 
 
