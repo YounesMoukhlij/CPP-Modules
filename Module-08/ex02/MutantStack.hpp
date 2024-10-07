e73abe9c1f2e87c418732eef9bc9ac9377f10e9e
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 15:19:29 +0100

    by gacp

--- Module-08/ex02/MutantStack.hpp
+++ Module-08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:52 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 15:19:00 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 15:19:28 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ class MutantStack : public std::stack<T>
 };
 
 template<class T>
-MutantStack<T>::MutantStack() : s
+MutantStack<T>::MutantStack() 
 {
 	std::cout << "Constructor Called." << std::endl;	
 }
@@ -46,7 +46,8 @@ MutantStack<T>::~MutantStack()
 	std::cout << "Destructor Called." << std::endl;	
 }
 
-MutantStack<T>::MutantStack(const MutantStack& origine)
+template<class T>
+MutantStack<T>::MutantStack(const MutantStack& origine) : std::stack<T>(origine)
 {
 	std::cout << "Copy Constructor Called." << std::endl;
 	if (this != &origine)
