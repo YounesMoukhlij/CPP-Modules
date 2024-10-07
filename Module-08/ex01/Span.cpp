a8d86e96108f86d5e2543cd9a1345e645eec337d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 11:46:29 +0100

    by gacp

--- Module-08/ex01/Span.cpp
+++ Module-08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 11:46:08 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 11:46:29 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,8 @@ int		Span::longestSpan()
 
 int    Span::addNumber(unsigned int i, unsigned int n)
 {
-	if (i < n)
-		_array[i]
+	if (i < _i)
+		_array[i] = n;
+	else
+	
 }
