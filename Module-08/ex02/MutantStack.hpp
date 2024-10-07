fe2d0fa52eaea21b673788326ee3995ebe3df1c2
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 13:41:57 +0100

    by gacp

--- Module-08/ex02/MutantStack.hpp
+++ Module-08/ex02/MutantStack.hpp
@@ -6,7 +6,25 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:52 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 11:32:52 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 13:41:57 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+class MutantStack
+{
+private:
+	/* data */
+public:
+	MutantStack(/* args */);
+	~MutantStack();
+};
+
+MutantStack::MutantStack(/* args */)
+{
+}
+
+MutantStack::~MutantStack()
+{
+}
