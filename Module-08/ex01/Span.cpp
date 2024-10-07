41665fd662b813754f5a3a006a0608740f21bcfb
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 12:00:44 +0100

    by gacp

--- Module-08/ex01/Span.cpp
+++ Module-08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 11:59:42 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 12:00:44 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,6 +83,7 @@ int    Span::addNumber(unsigned int n)
 
 void	Span::gimmeSomeGaz(unsigned int _n, unsigned int _i)
 {
+	_i = _
 	for (unsigned int index = 0x0; index < _i; index++)
 	{
 		_array[index] = 
