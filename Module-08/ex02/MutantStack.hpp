70fc710ea0b5b5691a187c6a50f8469b6687ea27
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 13:43:38 +0100

    by gacp

--- Module-08/ex02/MutantStack.hpp
+++ Module-08/ex02/MutantStack.hpp
@@ -6,19 +6,26 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:52 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 13:42:01 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 13:43:33 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+# include <iostream>
+# include <stdexcept>
+# include <exception>
+# include <algorithm>
+
+# define IGNORE(param) (void)(param)
+
 class MutantStack
 {
-private:
-	/* data */
-public:
-	MutantStack(/* args */);
-	~MutantStack();
+	private:
+		/* data */
+	public:
+		MutantStack(/* args */);
+		~MutantStack();
 };
 
 MutantStack::MutantStack(/* args */)
