ebe2af4557bd899111af40990cd7fcefd9720e0a
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 12:41:29 +0100

    by gacp

--- Module-08/ex01/Span.cpp
+++ Module-08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 12:33:04 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 12:41:17 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,7 +89,6 @@ int		Span::longestSpan()
 	for (unsigned int i = 0; i< _n ; i++)
 	{
 		std::cout << _array[i] << std::endl;
-	
 	}
 	std::sort(_array, _array + _n);
 	
