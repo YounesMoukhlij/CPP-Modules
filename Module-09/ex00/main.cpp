c7cd758d9950a489a54352e608c8123d70ac0c9d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 16:40:48 +0100

    by gacp

---
+++ Module-09/ex00/main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 16:40:33 by youmoukh          #+#    #+#             */
+/*   Updated: 2024/10/07 16:40:47 by youmoukh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main(int you, char **nes)
+{
+	IGNORE(nes);
+	if (you != 0x1)
+	{
+		std::cerr << "The Program Does not take any Arguments." << std::endl;
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
