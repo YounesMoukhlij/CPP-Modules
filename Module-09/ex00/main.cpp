0ca8beaebef44bfbfb21524a83087a7ef2555a6d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 16:50:33 +0100

    by gacp

--- Module-09/ex00/main.cpp
+++ Module-09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:40:33 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 16:50:10 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 16:50:33 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int main(int you, char **nes)
 	IGNORE(nes);
 	if (you < 0x2)
 	{
-		std::cerr << "The Program Does not take any Arguments." << std::endl;
+		std::cerr << "Usage ./btc <fil." << std::endl;
 		return (EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
