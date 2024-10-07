78162faf1b68e98905c87e32953e7ce4b7a84ee0
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 13:42:56 +0100

    by gacp

--- Module-08/ex02/main.cpp
+++ Module-08/ex02/main.cpp
@@ -6,8 +6,41 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 13:42:32 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 13:42:36 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 13:42:55 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include ""
+#include "MutantStack.hpp"
+
+
+int main(int you, char **nes)
+{
+	IGNORE(nes);
+	if (you != 0x1)
+	{
+		std::cerr << "The Program Does not take any Arguments." << std::endl;
+		return (EXIT_FAILURE);
+	}
+MutantStack<int> mstack;
+mstack.push(5);
+mstack.push(17);
+std::cout << mstack.top() << std::endl;
+mstack.pop();
+std::cout << mstack.size() << std::endl;
+mstack.push(3);
+mstack.push(5);
+mstack.push(737);
+//[...]
+mstack.push(0);
+MutantStack<int>::iterator it = mstack.begin();
+MutantStack<int>::iterator ite = mstack.end();
+++it;
+--it;
+while (it != ite)
+{
+std::cout << *it << std::endl;
+++it;
+}
+std::stack<int> s(mstack);
+return 0;
+}
