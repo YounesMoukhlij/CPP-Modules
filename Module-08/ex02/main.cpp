8e1f74a65eef52ff604e791fdae11ddb16af1e99
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 16:24:28 +0100

    by gacp

--- Module-08/ex02/main.cpp
+++ Module-08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 13:42:32 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 16:24:07 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 16:24:27 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,10 @@ int main(int you, char **nes)
 	MutantStack<int>::iterator t2 = test.end();
 	std::cout << "Begin of test "<< *t1 << std::endl;
 	std::cout << "End of test " << *t2 << std::endl;
-	for (MutantStack<int>::iterator i = test.begin(); i < test.end())
+	for (MutantStack<int>::iterator i = test.begin(); i < test.end(); i++)
+	{
+		std::cout << "tes"
+	}
 	
 	
 	return (EXIT_SUCCESS);
