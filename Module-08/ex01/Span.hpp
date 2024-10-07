4d608476e680aec895cbf7a9dbbc4229e641e29b
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 12:47:23 +0100

    by gacp

--- Module-08/ex01/Span.hpp
+++ Module-08/ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:24 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 12:07:04 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 12:47:20 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,5 @@ class Span
 		int		longestSpan();
 		void	addNumber(unsigned int n);
 		void	arrayFiller();
+		void	diplayArray();
 };
