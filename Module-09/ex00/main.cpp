d074d47fd53ebb954dbcc07b95def7e7a84d7131
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 16:50:13 +0100

    by gacp

--- Module-09/ex00/main.cpp
+++ Module-09/ex00/main.cpp
@@ -6,14 +6,14 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:40:33 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 16:40:47 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 16:50:10 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int main(int you, char **nes)
 {
 	IGNORE(nes);
-	if (you != 0x1)
+	if (you < 0x2)
 	{
 		std::cerr << "The Program Does not take any Arguments." << std::endl;
 		return (EXIT_FAILURE);
