890f0be4a408633209062830d40329c31b7982c7
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 11:56:11 +0100

    by gacp

--- Module-08/ex01/Span.cpp
+++ Module-08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 11:55:34 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 11:56:11 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,8 +65,8 @@ int		Span::shortestSpan()
 int		Span::longestSpan()
 {
 	if (_n <= 0x1)
-		throw std::out_of_range("No Enough numbers to look in.")
-	std::sort()
+		throw std::out_of_range("No Enough numbers to look in.");
+	std::sort(_array[0x0, _ar])
 }
 
 int    Span::addNumber(unsigned int n)
