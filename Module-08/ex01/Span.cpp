0774cf86163a975a9b2137e997b5de12ba7472ce
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 11:37:40 +0100

    by gacp

--- Module-08/ex01/Span.cpp
+++ Module-08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 11:32:08 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 11:37:34 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 
 Span::Span()
 {
-
+	_array = 0x0;
+	_n = 0x0;
 }
 
 Span::Span(unsigned int n)
@@ -34,7 +35,14 @@ Span::~Span()
 
 Span&   Span::operator=(const Span& origine)
 {
-
+	_array = 0x0;
+	if (this != &origine)
+	{
+		_size = origine._size;
+		_array = new T[_size];
+		for (std::size_t i = 0x0; i < _size ; i++)
+			_array[i] = origine._array[i];
+	}
 }
 
 
