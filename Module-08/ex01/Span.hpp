7dcfbc8e66a7ef981d59abbde07f1f8f84102e67
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 11:48:28 +0100

    by gacp

--- Module-08/ex01/Span.hpp
+++ Module-08/ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:24 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 11:42:46 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 11:48:17 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,5 @@ class Span
 		Span&   operator=(const Span& origine);
 		int		shortestSpan();
 		int		longestSpan();
-        int     addNumber();
+		int		addNumber(unsigned int n)
 };
