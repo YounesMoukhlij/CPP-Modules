af311d75fdb8181f0562071986c3c9f98a03e16f
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 12:10:29 +0100

    by gacp

--- Module-08/ex01/main.cpp
+++ Module-08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:11 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 12:10:10 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 12:10:16 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int main(int you, char **nes)
 	Span a(3);
 	
 	a.arrayFiller();
-	std::cout << a.Lo
+	std::cout << a.longestSpan() << std::endl;
 
 	return (EXIT_SUCCESS);
 }
