49767f2196c4b0eedb7dac21a79f817e6f5bfd9c
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 16:41:09 +0100

    by gacp

--- Module-09/ex00/BitcoinExchange.hpp
+++ Module-09/ex00/BitcoinExchange.hpp
@@ -6,9 +6,16 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:39:06 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 16:40:20 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 16:40:56 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+# include <iostream>
+# include <stdexcept>
+# include <exception>
+# include <algorithm>
+# include <stack>
+
+# define IGNORE(param) (void)(param)
