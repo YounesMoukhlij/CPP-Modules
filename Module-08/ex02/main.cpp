dbb2e8ae44bf517196a322884b0e7a60fff74617
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 16:07:46 +0100

    by gacp

--- Module-08/ex02/main.cpp
+++ Module-08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 13:42:32 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 16:07:10 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 16:07:37 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ int main(int you, char **nes)
 		std::cout << *it << std::endl;
 		++it;
 	}
-	std::stack<int> s(mstack);
+	MutantStack<int> s(mstack);
 	MutantStack<int>::iterator io = s.begin();
 	MutantStack<int>::iterator ioe = s.end();
 	++io;
