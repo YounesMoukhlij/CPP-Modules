55b820e7ea395fad6ea9d8894f9e8dc1e4eec107
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 16:08:06 +0100

    by gacp

--- Module-08/ex02/main.cpp
+++ Module-08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 13:42:32 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 16:07:37 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 16:08:06 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,10 @@ int main(int you, char **nes)
 	
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
+	std::cout << "TOP : " << mstack.top() << std::endl;
 	mstack.pop();
 	std::cout << mstack.size() << std::endl;
+	std::cout << "TOP : " << mstack.top() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
