d828cd61289a5be0d78fc9b6752c737f65cd31c5
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-07 13:16:39 +0100

    by gacp

--- Module-08/ex01/main.cpp
+++ Module-08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:11 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 12:52:11 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/07 13:16:25 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int main(int you, char **nes)
 		std::cerr << "The Program Does not take any Arguments." << std::endl;
 		return (EXIT_FAILURE);
 	}
-	Span a(6);
+	Span a(1);
 	
 	a.arrayFiller();
 	a.displayArray();
