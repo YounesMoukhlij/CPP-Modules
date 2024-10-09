4e76e07e81ea236ca980086393d1bbb1feec668d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-09 14:33:51 +0100

    by gacp

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:39:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/09 14:33:34 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/09 14:33:44 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,8 +103,8 @@ void	BitcoinExchange::loadData(void)
 	// size_t		delPos;
 
 	std::getline(dataBase, line);
+	line = trim (line);
 	std::cout  << line << std::endl;
-	// line = trim (line);
 	// if (line != "date,exchange_rate")
 	// 	throw	InvalidData();
 	// while (std::getline(dataBase, line))
