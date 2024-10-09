dcd338ad1109945e035906743b93c83d9b5fe196
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-09 12:38:55 +0100

    by gacp

--- Module-09/ex00/main.cpp
+++ Module-09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:40:33 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/09 12:35:22 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/09 12:38:55 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int main(int you, char **nes)
 	try
 	{
 		std::string file = static_cast<const std::string>(nes[0x1]);
-		bit.NameChecker(file, 0x0);
+		bit.ameChecker(file, 0x0);
 	}
 	catch (std::exception &younes)
 	{
