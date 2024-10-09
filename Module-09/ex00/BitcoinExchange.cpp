a82f8a664a04711f9acc8e23193f1ce28df78426
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-09 17:38:24 +0100

    by gacp

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:39:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/09 17:38:09 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/09 17:38:24 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -193,7 +193,7 @@ bool	BitcoinExchange::parseEntry(std::string& str)
 		return false;
 	if (_month == 2)
 	{
-		if (day > 28 )
+		if (_day > 28 || _day >  )
 			return false;
 	}
 	/*
