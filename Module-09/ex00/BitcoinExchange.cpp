6c98e08421df4e2e1c3c18e4df51b2b7b0bdb77f
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-09 16:14:48 +0100

    by gacp

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:39:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/09 16:14:28 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/09 16:14:47 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,6 +108,7 @@ void	BitcoinExchange::loadData(void)
 		rate = trim(rate);
 		std::stringstream	_Float(rate);
 		_Float  >> rateV;
+		std::cout << rate
 		if (_Float.fail() || !_Float.eof())
 		{
 			infile.close();
