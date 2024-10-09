16eeba1eede3d116a0e355f76bb0469cd07619c4
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-09 16:50:42 +0100

    by gacp

--- Module-09/ex00/BitcoinExchange.hpp
+++ Module-09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:39:06 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/09 16:50:21 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/09 16:50:24 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,8 @@ class BitcoinExchange
 		void				readData();
 		void				fileDescriptors();
 		bool				parseEntry(std::string& str);
-		void				nameChecker(std::string& name, int i);
 		void				fillData(const std::string& infos);
+		void				nameChecker(std::string& name, int i);
 		void				openingFiles(const std::string& name, int j);
 		
 		
