2bd026fc559a4a844ebe66885a61aa38061bc5b1
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-09 14:05:59 +0100

    by gacp

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:39:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/09 14:05:37 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/09 14:05:55 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,7 +95,7 @@ std::string trim(const std::string& str)
 	while (last > first && std::isspace(str[last - 1]))
 		last--;
 
-	return str.substr(first, first - first);
+	// return str.substr(first, first - first);
 
     return str.substr(first, last - first + 1);
 }
