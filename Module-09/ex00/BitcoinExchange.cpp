784435c670f2759384dc065c0fce7350fb4de378
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-09 16:08:53 +0100

    by gacp

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:39:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/09 16:08:40 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/09 16:08:53 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,7 +117,7 @@ void	BitcoinExchange::loadData(void)
 		dataMap[date] = rateV;
 	}
 	// print map
-	for (std::map<std::string, float>::const_iterator it = dataMap.be)
+	for (std::map<std::string, float>::const_iterator it = dataMap.begin() ; it < dataMap)
 
 	
 }
