a02ed4e21dc9c2206a722976bab758c77dd41882
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-09 12:19:20 +0100

    by gacp

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -6,8 +6,11 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:39:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/07 16:40:12 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/09 12:19:17 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
+
+
+void	BitcoinExchange::NameChecker(std::string )
