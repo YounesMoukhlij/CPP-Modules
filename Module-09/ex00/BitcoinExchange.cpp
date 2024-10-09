e192d3a65495b3dfb93417a866608f7830a85de3
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-09 14:57:40 +0100

    by gacp

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:39:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/09 14:57:26 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/09 14:57:36 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,21 +84,6 @@ std::string trim(const std::string& str)
     return str.substr(first, last - first + 1);
 }
 
-
-void	BitcoinExchange::addToDataBaseMap(const std::string& _str, size_t _posDelimiter)
-{
-	std::string	_date;
-	std::string	_rate;
-	float		_rateValue;
-
-	_date = _str.substr(0, _posDelimiter);
-	_rate = _str.substr(_posDelimiter + 1);
-	_date = trimString(_date);
-	_rate = trimString(_rate);
-	std::stringstream	rateFloat(_rate);
-	rateFloat >> _rateValue;
-
-}
 
 void	BitcoinExchange::loadData(void)
 {
