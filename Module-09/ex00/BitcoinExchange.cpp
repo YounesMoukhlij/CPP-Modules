0bb5ca667ce66e319c97ed43b46e3db688e8a225
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-09 12:20:45 +0100

    by gacp

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:39:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/09 12:20:30 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/09 12:20:31 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	BitcoinExchange::NameChecker(std::string& name)
 		throw InvalidName();
 	}
 	Extention = name.substr(name.size() - 4);
-	if (Extention != target1 && fileExtension != target2) {
+	if (Extention != target1 && Extention != target2) {
 		std::cerr << name << ": ";
 		throw InvalidName();
 	}
