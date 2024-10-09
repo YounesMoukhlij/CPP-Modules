2ba5938d428f1129ee56a10cb4baa7831bf1eb8d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-09 12:39:35 +0100

    by gacp

--- Module-09/ex00/BitcoinExchange.hpp
+++ Module-09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:39:06 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/09 12:39:09 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/09 12:39:30 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@
 class BitcoinExchange
 {
 	private:
-		std::iifile;
+		stinfile;
+		std::ifstream					dataBase;
 	public :
 		BitcoinExchange();
 		~BitcoinExchange();
