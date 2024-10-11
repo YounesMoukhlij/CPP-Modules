75ec50695180118753c6effbb69bb01ab3728886
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 14:12:41 +0100

    by gacp

--- Module-05/ex03/RobotomyRequestForm.cpp
+++ Module-05/ex03/RobotomyRequestForm.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:51:45 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/10 16:49:24 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 14:12:39 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,13 @@
 RobotomyRequestForm::RobotomyRequestForm(void) : AForm("Robot", 0x48, 0x2D)
 {
 	std::cout << "Robotomy default Constructor Called." << std::endl;
-	
 	this->_target = "defaultTarget";
-	std::srand(std::time(0x0));
 }
 
 RobotomyRequestForm::RobotomyRequestForm(const std::string _target): AForm(_target, 0x48, 0x2D)
 {
 	std::cout << "Robotomy parametriesed Constructor Called." << std::endl;
 	this->_target = _target;
-	std::srand(std::time(0x0));
 }
 
 RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm& origine) : AForm(origine.getName(), origine.getGradeSigned(), origine.getGradeExecute())
@@ -32,7 +29,6 @@ RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm& origine) : A
 	std::cout << "Robotomy Copy Constructor Called." << std::endl;
 	if (this != &origine)
 		*this = origine;
-	std::srand(std::time(0x0));
 }
 
 RobotomyRequestForm::~RobotomyRequestForm(void)
@@ -57,6 +53,7 @@ const std::string& RobotomyRequestForm::getTarget(void) const
 void	RobotomyRequestForm::execute(Bureaucrat const & bur) const
 {
 	executionChecker(bur);
+	std::srand(std::time(0x0));
 	std::cout << bur.getName() << " executed " << this->_target << std::endl;
 	std::cout << "Drilling noises..." << std::endl;
 	if (std::rand() % 0x2)
