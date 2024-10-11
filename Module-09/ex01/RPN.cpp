2d3f6293edd07ce7a591878c87ae0ef86c3144b0
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:19:20 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:18:51 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:19:15 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,7 +102,8 @@ int	RPN::strToInt(const std::string& _array) const
 	return (_value);
 }
 
-// void	RPN::isOperationValid(int firstValue, int secondValue, char _operator) const {
+// void	RPN::isOperationValid(int firstValue, int secondValue, char _operator) const
+// {
 // 	switch (_operator)
 // 	{
 // 		case '*':
