b7182d31d5027b71e05d5fb1c34bb588d88ec5b2
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 18:11:27 +0100

    by gacp

--- Module-09/ex01/RPN.hpp
+++ Module-09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:39 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 18:11:10 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 18:11:12 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ class RPN
 		
 		// Member Functions
 		int			ValidArg(char _oprt) const;
-		void		PolonaiseInverse(cstd::string& _array);
+		void		PolonaiseInverse(const std::string& _array);
 		int			OperatorChecker( std::string& _array) ;
 		int			IntConversion(const std::string& _array) const;
 		void		ParseArguments(const std::string& _array) const;
