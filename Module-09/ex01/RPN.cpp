7d2d2717d59e4a3d25802ddb778fd74bf63f35eb
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 17:37:28 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 17:36:23 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 17:37:23 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -146,7 +146,7 @@ int	RPN::OperatorChecker( std::string& _array)
 // }
 
 
-void	RPN::CalculateStaff(std::string& str, std::stack<int>& _stack)
+void	RPN::CalculateStaff(int opr, std::stack<int>& _stack)
 {
 	(void) str;
 	(void) _stack;
