398938e31916a4a014de2791dcfd6b6e1e994a75
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:46:50 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:41:36 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:46:49 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,6 +96,7 @@ void	RPN::CheckFlow(int Value_1, int Value_2, char opr) const
 			if (Value_1 != 0x0 && Value_2 != 0x0)
 				throw ErrorOperation();
 			if (Value_1 * Value_2 > INT_MAX || Value_1 * Value_2 < INT_MIN)
+				throw std::invalid_argument("Division by zero");
 				throw ErrorFlow();
 			break;
 		}
