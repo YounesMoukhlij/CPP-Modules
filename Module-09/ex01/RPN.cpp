14127232d8a7cb528ceb23951c91d4de95ce85f0
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:15:09 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:14:59 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:15:09 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,7 +79,8 @@ int	RPN::OperatorChecker( std::string& _array)
 	
 	for (int i = 0x0; i < 0x4; i++)
 	{
-		if (tmp.compare(o))
+		if (_ar)
+		if (tmp.compare(oprt[i]))
 			indicator = i;
 	}
 	switch(indicator)
