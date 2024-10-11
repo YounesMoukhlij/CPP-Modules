22662112db6402f6d1618d401af71d3c8c94547e
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:28:47 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:28:36 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:28:40 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -114,6 +114,7 @@ void	RPN::CheckFlow(int Value_1, int Value_2, char opr) const
 			if (Value_1 < 0 && Value_2 > INT_MAX + Value_1)
 				throw ErrorFlow();
 			break;
+		}
 	}
 }
 
