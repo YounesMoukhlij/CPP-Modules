31b4d78ca99f14ab118d9db16a14cf883fa11a30
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:22:19 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:21:39 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:22:14 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ RPN& RPN::operator=(const RPN& origine)
 
 bool	RPN::ValidArguments(const std::string& _array, size_t length) const
 {
-	static size_t	_countDigit;
+	static size_t	_Dcounter;
 	static size_t	_countOperators;
 
 	for (size_t i = 0x0; i < length; i++)
