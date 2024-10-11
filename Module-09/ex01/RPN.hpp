936d2a038871bd3a73c50ac699029b3f2f9e9970
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:05:46 +0100

    by gacp

--- Module-09/ex01/RPN.hpp
+++ Module-09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:39 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:05:33 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:05:45 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,10 @@ class RPN
 		RPN(const RPN& origine);
 		RPN& operator=(const RPN& origine);
 		
-		int		OperatorChecker(const std::string& array) const
-		
-		void		ParseArguments(const std::string& array) const;
+		// <
 		void		PolonaiseInverse(std::string& array);
+		int			OperatorChecker(const std::string& array) const;
+		void		ParseArguments(const std::string& array) const;
 		
 
 		class	ErrorArgument : public std::exception
