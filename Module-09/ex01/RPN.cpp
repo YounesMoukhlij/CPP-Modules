4fc8a6cd3413135d2bf0c34d7c351589dc9222f4
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:23:18 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:23:00 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:23:13 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,32 +82,32 @@ int	RPN::IntConversion(const std::string& _array) const
 	return (_intValue);
 }
 
-// void	RPN::isOperationValid(int firstValue, int secondValue, char _operator) const
-// {
-// 	switch (_operator)
-// 	{
-// 		case '*':
-// 			if (firstValue != 0 && secondValue != 0 &&
-// 				(firstValue * secondValue > INT_MAX || firstValue * secondValue < INT_MIN))
-// 				throw ErrorOverFlow;
-// 			break;
-// 		case '/':
-// 			if (firstValue == 0)
-// 				throw std::invalid_argument("Division by zero");
-// 			if (secondValue == INT_MIN && firstValue == -1)
-//  				throw ErrorOverFlow;
-// 			break;
-// 		case '+':
-// 			if ((firstValue > 0 && secondValue > INT_MAX - firstValue) ||
-// 				(firstValue < 0 && secondValue < INT_MIN - firstValue))
-// 				throw ErrorOverFlow;
-// 			break;
-// 		case '-':
-// 			if (firstValue < 0 && secondValue > INT_MAX + firstValue)
-// 				throw ErrorOverFlow;
-// 			break;
-// 	}
-// }
+void	RPN::isOperationValid(int firstValue, int secondValue, char _operator) const
+{
+	switch (_operator)
+	{
+		case '*':
+			if (firstValue != 0 && secondValue != 0 &&
+				(firstValue * secondValue > INT_MAX || firstValue * secondValue < INT_MIN))
+				throw ErrorOverFlow;
+			break;
+		case '/':
+			if (firstValue == 0)
+				throw std::invalid_argument("Division by zero");
+			if (secondValue == INT_MIN && firstValue == -1)
+ 				throw ErrorOverFlow;
+			break;
+		case '+':
+			if ((firstValue > 0 && secondValue > INT_MAX - firstValue) ||
+				(firstValue < 0 && secondValue < INT_MIN - firstValue))
+				throw ErrorOverFlow;
+			break;
+		case '-':
+			if (firstValue < 0 && secondValue > INT_MAX + firstValue)
+				throw ErrorOverFlow;
+			break;
+	}
+}
 
 
 
