939645b3c8e29dc97511bde1629267e0735111ba
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 14:31:33 +0100

    by gacp

--- Module-05/ex03/main.cpp
+++ Module-05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:05 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 14:29:49 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 14:31:30 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,9 @@ int main(int you, char **nes)
 	Intern B;
 
 	A = B;
+	
+	Bureaucrat bureaucrat("Younes", 1);
+	
 	try
 	{
 		Bureaucrat bureaucrat("Younes", 1);
