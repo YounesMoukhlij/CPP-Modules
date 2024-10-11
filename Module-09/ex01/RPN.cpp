634ff6a2fadd4ca7c9d7a1ac9ae139ac903c1fa1
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:28:58 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:28:40 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:28:55 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,7 +94,7 @@ void	RPN::CheckFlow(int Value_1, int Value_2, char opr) const
 				throw ErrorFlow();
 			break;
 		}
-		case D:
+		case DI:
 		{
 			if (Value_1 == 0)
 				throw std::invalid_argument("Division by zero");
--- Module-09/ex01/RPN.hpp
+++ Module-09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:39 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:27:49 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:28:50 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@
 # define M '*'
 # define A '+'
 # define S '-'
-# define D '\'
+# define DI '\'
 
 class RPN
 {
