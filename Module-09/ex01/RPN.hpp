16a7dda506ae270e28a383be7cef955343559b11
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 18:34:36 +0100

    by gacp

--- Module-09/ex01/RPN.hpp
+++ Module-09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:39 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 18:16:04 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 18:34:27 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@
 # define M '*'
 # define A '+'
 # define S '-'
-# define D '\''
+# define D '/'
 # define SPACE ' '
 
 class RPN
