bcee39d1abd1c6c427e7fb8d3764edc63a3a7772
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:58:02 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:57:32 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:57:59 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -125,7 +125,6 @@ void	RPN::CheckFlow(int Value_1, int Value_2, char opr) const
                 throw ErrorFlow();
             break;
         }
-        
         case S: 
         {
         
@@ -135,7 +134,6 @@ void	RPN::CheckFlow(int Value_1, int Value_2, char opr) const
                 throw ErrorFlow();
             break;
         }
-
         case D: 
         {
         
