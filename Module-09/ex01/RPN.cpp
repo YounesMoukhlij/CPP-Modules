30a049a69ea8598073a4d2e272249c6b045ef8ef
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:25:11 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:24:39 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:25:11 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,8 +87,8 @@ void	RPN::CheckFlow(int Value_1, int Value_2, char opr) const
 	switch (opr)
 	{
 		case '*':
-			if (Value_1 != 0 && Value_2 != 0 &&
-				(Value_1 * Value_2 > INT_MAX || Value_1 * Value_2 < INT_MIN))
+			if (Value_1 != 0 && Value_2 != 0)
+			if (Value_1 * Value_2 > INT_MAX || Value_1 * Value_2 < INT_MIN))
 				throw ErrorFlow();
 			break;
 		case '/':
