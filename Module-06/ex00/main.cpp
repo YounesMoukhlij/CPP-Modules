6afb4d8523dc493f49b844bd468e8b9cdbb15bc6
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 15:19:46 +0100

    by gacp

--- Module-06/ex00/main.cpp
+++ Module-06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:55:06 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/04 18:30:21 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 15:19:42 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@ int main(int you, char **nes)
 {
 	IGNORE(nes);
 
-	if (you != 2)
+	if (you != 0x2)
 	{
 		std::cerr << "Try: ./convert <arg must be a number>" << std::endl;
 		return (EXIT_FAILURE);
 	}
+	
 	ScalarConverter::convert(static_cast<std::string>(nes[1]));
 	return (EXIT_SUCCESS);
 }
