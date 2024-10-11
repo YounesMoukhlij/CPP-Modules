9cd26fa5b8b0162da5e080c40beb8ab75648fb4f
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:48:19 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:48:08 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:48:17 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,8 +66,8 @@ bool	RPN::ValidArguments(const std::string& _array, size_t length) const
 	static size_t	_Dcounter;
 	static size_t	_Ocounter;
 	
-	if (!isdigit(static_cast<int>(_array[0]))
-		throw
+	if (!isdigit(static_cast<int>(_array[0x0])))
+		throw 
 	for (size_t i = 0x0; i < length; i++)
 	{
 		// first number 
