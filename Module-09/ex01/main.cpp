a44e8a465c20cf04a57d1375b543395a1b45a8ed
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 15:46:22 +0100

    by gacp

--- Module-09/ex01/main.cpp
+++ Module-09/ex01/main.cpp
@@ -6,9 +6,28 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:43:23 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 15:45:08 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 15:46:17 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
+int main(int ac, char **av) {
+
+	(void)av;
+	std::string	_str;
+	RPN	test;
+	if (ac != 2) {
+		std::cerr << "Error usage: ./RPN 'arg'" << std::endl;
+		return (1);
+	}
+	_str = static_cast<const std::string> (av[1]);
+	try
+	{
+		test.parseArg(_str);
+		test.ReversePolish(_str);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error: " << e.what() << " => "<< av[1] << std::endl;
+	}
