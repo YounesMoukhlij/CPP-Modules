14fd54aed6c73264aea636f03c82105834ebc669
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:01:38 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:01:28 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:01:31 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,6 +92,8 @@ int	RPN::whichOperator(const std::string& array) const
 	switch(indicator)
 	{
 		case 0x0:
+			return (0x1);
+		case 0x1:
 			return (0x1);
 		case 0x0:
 			return (0x1);
