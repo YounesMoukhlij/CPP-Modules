e19fc9edd3ef8b27295f6d1682f691db813dad67
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 15:57:27 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 15:57:16 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 15:57:24 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,7 +81,7 @@ bool	RPN::isValidChracters(const std::string& _str) const {
 
 int	RPN::whichOperator(const std::string& _str) const
 {
-	std::string oprt = ["*", ""]
+	std::string oprt = ["*", "/", "+", "-"]
 	if (_str[0] == '*')
 		return (1);
 	else if (_str[0] == '/')
