4ed00ba917191bdec1099138c66030ba2a472e45
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 17:54:46 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 17:54:33 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 17:54:46 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -152,6 +152,7 @@ void	RPN::CalculateStaff(int opr, std::stack<int>& _stack)
 	(void) _stack;
 	
 	// if operator then do simple math
+	for (std::stac)
 	switch(opr)
 	{
 		case  MULT:
