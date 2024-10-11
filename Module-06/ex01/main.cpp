da6d7b9a86521b0e374a7173bfc3422151ee2112
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 14:35:54 +0100

    by gacp

--- Module-06/ex01/main.cpp
+++ Module-06/ex01/main.cpp
@@ -6,23 +6,23 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:55:06 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 13:41:31 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 14:35:50 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
 
-// class younes
-// {
-// 	private:
-// 		younes() {};
-// 	public:
-// 		~younes() {};
-// 		 void print()
-// 		{
-// 			std::cout << "hello" << std::endl;
-// 		}
-// };
+class younes
+{
+	private:
+		younes() {};
+	public:
+		~younes() {};
+		 void print()
+		{
+			std::cout << "hello" << std::endl;
+		}
+};
 
 
 int main(int you, char **nes)
@@ -33,36 +33,36 @@ int main(int you, char **nes)
 		std::cerr << "The Program Does not take any Arguments." << std::endl;
 		return (EXIT_FAILURE);
 	}
-	// uintptr_t	serialized;
-	// Data		*deserializedData;
-	// Data 		data;
+	uintptr_t	serialized;
+	Data		*deserializedData;
+	Data 		data;
 
-	// // inialisation.
-	// data._boolValue = false;
-	// data._charValue = 'h';
-	// data._floatValue = 199.442f;
-	// data._intValue = 13;
+	// inialisation.
+	data._boolValue = false;
+	data._charValue = 'h';
+	data._floatValue = 199.442f;
+	data._intValue = 13;
 	
-	// printf("size of %lu\n", sizeof(uintptr_t));
+	printf("size of %lu\n", sizeof(uintptr_t));
 	
-	// serialized = Serializer::serialize(&data);
-	// deserializedData = Serializer::deserialize(serialized);
-	// try
-	// {
-	// 	if (deserializedData == &data)
-	// 	{
-	// 		std::cout << "Operation Done Successfully :)" << std::endl;
-	// 		std::cout << "intValue: " << deserializedData->_intValue << std::endl;
-	// 		std::cout << "charValue: " << deserializedData->_charValue << std::endl;
-	// 		std::cout << "boolValue: " << deserializedData->_boolValue << std::endl;
-	// 		std::cout << "floatValue: " << deserializedData->_floatValue << std::endl;
-	// 	}
-	// 	else
-	// 		throw Serializer::ExceptionSerializer();
-	// }
-	// catch (std::exception& younes)
-	// {
-    //     std::cout << "Caught exception: " << younes.what() << std::endl;
-	// }
+	serialized = Serializer::serialize(&data);
+	deserializedData = Serializer::deserialize(serialized);
+	try
+	{
+		if (deserializedData == &data)
+		{
+			std::cout << "Operation Done Successfully :)" << std::endl;
+			std::cout << "intValue: " << deserializedData->_intValue << std::endl;
+			std::cout << "charValue: " << deserializedData->_charValue << std::endl;
+			std::cout << "boolValue: " << deserializedData->_boolValue << std::endl;
+			std::cout << "floatValue: " << deserializedData->_floatValue << std::endl;
+		}
+		else
+			throw Serializer::ExceptionSerializer();
+	}
+	catch (std::exception& younes)
+	{
+        std::cout << "Caught exception: " << younes.what() << std::endl;
+	}
 	return (EXIT_SUCCESS);
 }
