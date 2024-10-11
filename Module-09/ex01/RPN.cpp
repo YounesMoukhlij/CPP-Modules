bfbb9e8b2797d41a0e67657182a6ff91e9c77bea
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:21:14 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:21:02 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:21:14 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,10 +72,10 @@ int	RPN::OperatorChecker( std::string& _array)
 	}
 }
 
-int	RPN::In(const std::string& _array) const
+int	RPN::IntConversion(const std::string& _array) const
 {
-	int					_value;
-	std::stringstream	strInt(_str);
+	int					value;
+	std::stringstream	strInt(_array);
 	strInt >> _value;
 	return (_value);
 }
