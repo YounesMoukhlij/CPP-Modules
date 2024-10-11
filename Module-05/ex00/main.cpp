9490fc2fdc925983df552f0a945ec4663167c6e4
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 13:57:18 +0100

    by gacp

--- Module-05/ex00/main.cpp
+++ Module-05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:05 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 13:57:04 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 13:57:14 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main(int you, char **nes)
 		return (EXIT_FAILURE);
 	}
 	
-	bureaucrat A("younes", 12);
+	Bureaucrat A("younes", 12);
 	bureaucrat B("moukhlij", 30);
 
 	B = A;
