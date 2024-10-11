dd1f0b8de8d6afccaec8ef4fc8bc80ca400a1529
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 14:32:14 +0100

    by gacp

--- Module-05/ex03/main.cpp
+++ Module-05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:05 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 14:32:04 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 14:32:08 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int main(int you, char **nes)
 	Bureaucrat AA("AA", 1);
 	Bureaucrat BB(AA);
 
-	std::cout << BB
+	std::cout << BB << std::endl;
 	
 	try
 	{
