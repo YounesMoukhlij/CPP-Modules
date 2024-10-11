a13f2ab1f08a4320c3d408b900f8749abcc52e86
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 14:10:20 +0100

    by gacp

--- Module-05/ex02/main.cpp
+++ Module-05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:05 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 14:10:06 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 14:10:18 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,61 +33,61 @@ int main(int you, char **nes)
 	std::cout << A << std::endl;
 	
 
-    std::cout << "1st Try" << std::endl;
-    std::cout << std::endl;
-	try
-	{
-		Bureaucrat bureaucrat("Mr", 20);
-		RobotomyRequestForm roboto("RRAATAATATA");
+    // std::cout << "1st Try" << std::endl;
+    // std::cout << std::endl;
+	// try
+	// {
+	// 	Bureaucrat bureaucrat("Mr", 20);
+	// 	RobotomyRequestForm roboto("RRAATAATATA");
 		
-		bureaucrat.signForm(roboto);
-		bureaucrat.executeForm(roboto);
-		std::cout << roboto;
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
+	// 	bureaucrat.signForm(roboto);
+	// 	bureaucrat.executeForm(roboto);
+	// 	std::cout << roboto;
+	// }
+	// catch (const std::exception& e)
+	// {
+	// 	std::cerr << e.what() << std::endl;
+	// }
 	
-    std::cout << std::endl;
-    std::cout << std::endl;
-    std::cout << "2nd Try" << std::endl;
-    std::cout << std::endl;
-    std::cout << std::endl;
+    // std::cout << std::endl;
+    // std::cout << std::endl;
+    // std::cout << "2nd Try" << std::endl;
+    // std::cout << std::endl;
+    // std::cout << std::endl;
 	
-	try
-	{
-		Bureaucrat bureaucrat("Younes", 100);
-		ShrubberyCreationForm shrubbery("Hayeler");
+	// try
+	// {
+	// 	Bureaucrat bureaucrat("Younes", 100);
+	// 	ShrubberyCreationForm shrubbery("Hayeler");
 		
-		bureaucrat.signForm(shrubbery);
-		bureaucrat.executeForm(shrubbery);
-		std::cout << shrubbery;
-	}
-	catch (const std::exception& e)
-	{
-			std::cerr << e.what() << std::endl;
-	}
+	// 	bureaucrat.signForm(shrubbery);
+	// 	bureaucrat.executeForm(shrubbery);
+	// 	std::cout << shrubbery;
+	// }
+	// catch (const std::exception& e)
+	// {
+	// 		std::cerr << e.what() << std::endl;
+	// }
 	
-	std::cout << std::endl;
-	std::cout << std::endl;
-    std::cout << "3rd Try" << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
+	// std::cout << std::endl;
+	// std::cout << std::endl;
+    // std::cout << "3rd Try" << std::endl;
+	// std::cout << std::endl;
+	// std::cout << std::endl;
 
-	try
-	{
-		std::srand(std::time(0x0));
-		Bureaucrat bureaucrat("YOUNAN", 6);
-		PresidentialPardonForm presidential("presidential");
-		bureaucrat.signForm(presidential);
-		bureaucrat.executeForm(presidential);
-		std::cout << presidential;
-	}
-	catch (const std::exception& e)
-	{
-			std::cerr << e.what() << std::endl;
-	}
+	// try
+	// {
+	// 	std::srand(std::time(0x0));
+	// 	Bureaucrat bureaucrat("YOUNAN", 6);
+	// 	PresidentialPardonForm presidential("presidential");
+	// 	bureaucrat.signForm(presidential);
+	// 	bureaucrat.executeForm(presidential);
+	// 	std::cout << presidential;
+	// }
+	// catch (const std::exception& e)
+	// {
+	// 		std::cerr << e.what() << std::endl;
+	// }
     return (EXIT_SUCCESS);
 }
         
