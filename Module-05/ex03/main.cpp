79c365a696618f140105338e046c41d026b006b1
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 14:32:54 +0100

    by gacp

--- Module-05/ex03/main.cpp
+++ Module-05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:05 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 14:32:08 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 14:32:54 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ int main(int you, char **nes)
 		std::cerr << "Program doesn't accepte any arguments" << std::endl;
 		return (EXIT_FAILURE);
 	}
+	
+	// Simple Tests
 	Intern A;
 	Intern B;
 
