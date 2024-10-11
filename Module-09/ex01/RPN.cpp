6cfab0143c13b49c7c9f958642f8488e1ca04b5d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 17:08:56 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 17:08:30 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 17:08:55 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -145,10 +145,10 @@ void	RPN::CheckFlow(int Value_1, int Value_2, char opr) const
 }
 
 
-// void	RPN::PolonaiseInverse(std::string& _array)
-// {
+void	RPN::PolonaiseInverse(std::string& _array)
+{
 
-// }
+}
 
 void	RPN::ParseArguments(const std::string& _array) const
 {
