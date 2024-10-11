03d4b9467a6bf3c66a23c7d1b2c8a950cbcb2774
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 18:34:14 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 18:34:03 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 18:34:10 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -191,7 +191,7 @@ void	RPN::CalculateStaff(int opr, std::stack<int>& _stack)
 		{
 			CheckFlow(num1, num2, '+');
 			std::cout << "++++" << std::endl;
-			_stack.push(num1 * num2);
+			_stack.push(num1 + num2);
 			break;
 
 		}
@@ -199,7 +199,7 @@ void	RPN::CalculateStaff(int opr, std::stack<int>& _stack)
 		{
 			CheckFlow(num1, num2, '-');
 			std::cout << "---" << std::endl;
-			_stack.push(num1 * num2);
+			_stack.push(num1 - num2);
 			break;
 
 		}
