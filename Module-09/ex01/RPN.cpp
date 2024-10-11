340423100c76f3b7e9b6fe949ff933ffca849ce5
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:30:37 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:30:21 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:30:34 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,7 +88,7 @@ void	RPN::CheckFlow(int Value_1, int Value_2, char opr) const
 	{
 		case M:
 		{
-			if (Value_1 != 0 && Value_2 != 0)
+			if (Value_1 != 0x0 && Value_2 != 0x0)
 				throw ErrorOperation();
 			if (Value_1 * Value_2 > INT_MAX || Value_1 * Value_2 < INT_MIN)
 				throw ErrorFlow();
@@ -96,7 +96,7 @@ void	RPN::CheckFlow(int Value_1, int Value_2, char opr) const
 		}
 		case D:
 		{
-			if (Value_1 == 0)
+			if (Value_1 == 0x0)
 				throw std::invalid_argument("Division by zero");
 			if (Value_2 == INT_MIN && Value_1 == -1)
  				throw ErrorFlow();
@@ -104,14 +104,14 @@ void	RPN::CheckFlow(int Value_1, int Value_2, char opr) const
 		}
 		case A:
 		{
-			if ((Value_1 > 0 && Value_2 > INT_MAX - Value_1) ||
-				(Value_1 < 0 && Value_2 < INT_MIN - Value_1))
+			if ((Value_1 > 0x0 && Value_2 > INT_MAX - Value_1) ||
+				(Value_1 < 0x0 && Value_2 < INT_MIN - Value_1))
 				throw ErrorFlow();
 			break;
 		}
 		case S:
 		{
-			if (Value_1 < 0 && Value_2 > INT_MAX + Value_1)
+			if (Value_1 < 0x0 && Value_2 > INT_MAX + Value_1)
 				throw ErrorFlow();
 			break;
 		}
