d6de470c1feacf872fe8546f817b67664bb28765
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:11:53 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:11:19 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:11:53 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,8 @@ int	RPN::OperatorChecker( std::string& _array)
 {
 	int	indicator = -0x1;
 	
-	 std::string oprt[4] = {"*", "/", "+", "-"};
+	std::string oprt[4] = {"*", "/", "+", "-"};
+	std::string	 
 	for (int i = 0x0; i < 0x4; i++)
 	{
 		if (_array[0] == oprt[0])
