772d8b33bdf3b4649fac033114e0e78244cde688
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 15:35:52 +0100

    by gacp

--- Module-09/ex01/RPN.hpp
+++ Module-09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:39 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 15:35:32 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 15:35:52 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,7 @@
 class RPN
 {
 	private:
-		std::stack<int>		rpnStack;
-		bool				isValidChracters(const std::string& _str) const;
-		bool				isOperator(char c) const;
-		bool				isSpace(char c) const;
-		int					strToInt(const std::string& _str) const;
-		int					whichOperator(const std::string& _str) const;
-		void				isOperationValid(int firstValue, int secondValue, char _operator) const;
-		void				operationCalcule(std::string& _piece, std::stack<int>& _stack);
+		std::stack<int>		stack;
 	public:
 		RPN();
 		~RPN();
