f7fa29126662d253d44310e630c95b228e46610d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:58:50 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:58:15 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:58:50 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,7 +81,8 @@ int	RPN::OperatorChecker( std::string& _array)
 }
 
 bool	RPN::ValidArguments(const std::string& _array, size_t length) const
-{	
+{
+	size_t 
 	if (!isdigit(static_cast<int>(_array[0x0])))
 		throw ErrorArgument();
 	for (size_t i = 0x0; i < length; i++)
