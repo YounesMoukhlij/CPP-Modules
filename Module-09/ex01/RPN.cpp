0009119582c5c14694b29c329ed6d80ffc01ebee
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:55:29 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:55:19 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:55:29 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,7 +87,8 @@ bool	RPN::ValidArguments(const std::string& _array, size_t length) const
 		throw ErrorArgument();
 	for (size_t i = 0x0; i < length; i++)
 	{
-		if (!isdigit(_array[i]) && !OperatorCharacter(_array[i])
+		if (!isdigit(_array[i]) && !OperatorCharacter(_array[i]))
+			throw ErrorArgument();
 	}
 	return (true);
 }
