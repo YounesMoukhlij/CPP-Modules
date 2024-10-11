749374fa7994526c2376a485bb63f92b559917a0
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 17:52:58 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 17:52:45 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 17:52:52 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -155,13 +155,21 @@ void	RPN::CalculateStaff(int opr, std::stack<int>& _stack)
 	switch(opr)
 	{
 		case (MULT - 0x1):
-			return (MULT);
+		{
+			
+		}
 		case (DIV - 0x1):
-			return (DIV);
+		{
+			
+		}
 		case (ADD - 0x1):
-			return (ADD);
+		{
+			
+		}
 		case (SOUS - 0x1):
-			return (SOUS);
+		{
+			
+		}
 		default :
 			throw ErrorArgument();
 	}
