5c54727376ea4fef7e5680f576648919c68a814a
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 14:09:57 +0100

    by gacp

--- Module-05/ex02/main.cpp
+++ Module-05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:38:05 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 14:09:40 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 14:09:57 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,12 @@ int main(int you, char **nes)
 	}
 	
 	// Testing Operator =
+	RobotomyRequestForm A("A");
+	RobotomyRequestForm B("B");
 	
+	A = 
 	
+
     std::cout << "1st Try" << std::endl;
     std::cout << std::endl;
 	try
