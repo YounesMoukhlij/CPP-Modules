86f6b5eab1d32d74f192855c0d66a9e3bc0bc7cc
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 14:40:08 +0100

    by gacp

--- Module-07/ex02/main.cpp
+++ Module-07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:42:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/05 18:59:22 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 14:40:07 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,6 +73,7 @@ int main(int you, char **nes)
         numbers[i] = rand();
     }
     delete [] mirror;
+	
 	std::cout << std::endl;
 	std::cout << std::endl;
 	std::cout << "2nd TEST" << std::endl;
