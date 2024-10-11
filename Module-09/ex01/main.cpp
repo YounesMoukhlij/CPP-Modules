b53365e245364a26c83492c9a0f52ad0b33fe3b5
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 15:47:33 +0100

    by gacp

--- Module-09/ex01/main.cpp
+++ Module-09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:43:23 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 15:47:22 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 15:47:33 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int main(int ac, char **av) {
 	if (ac != 2)
 	{
 		std::cerr << "Error usage: ./RPN 'arg'" << std::endl;
-		return ();
+		return (EXIT_FAILURE);
 	}
 	
 	RPN	test;
@@ -32,3 +32,5 @@ int main(int ac, char **av) {
 	{
 		std::cerr << "Error: " << e.what() << " => "<< av[1] << std::endl;
 	}
+
+}
