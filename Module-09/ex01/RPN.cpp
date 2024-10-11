ef4ac9079ccc7b114e5a9d067edcffe5ef9a8885
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 17:11:03 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 17:10:49 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 17:11:01 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -171,7 +171,7 @@ void	RPN::ParseArguments(const std::string& _array) const
 		if (!isdigit(_array[i]) && _array[i] != ' ')
 			Cnumbers++;
 	}
-	if (!(Coperators - 0x1 == Cnumbers))
+	if (!(Coperators + 0x1 == Cnumbers))
 		throw ErrorArgument();
 }
 
