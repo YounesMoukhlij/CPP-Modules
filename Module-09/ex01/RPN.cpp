f42cd66ffcf18ff940c3961912f793f1b4c74bab
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-11 16:06:57 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 16:06:22 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/11 16:06:55 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -177,7 +177,7 @@ void	RPN::PolonaiseInverse(std::string& array)
 
 void	RPN::ParseArguments(const std::string& array) const
 {
-	if (!isdigit(static_cast<int> (array[0])))
+	if (!isdigit(static_cast<int>(array[0])))
 		throw ErrorArgument();
 	if (!isValidChracters(array))
 		throw ErrorArgument();
