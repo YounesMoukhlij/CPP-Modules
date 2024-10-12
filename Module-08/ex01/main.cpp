0045c62c718bbc55187f5af149b5c2ffa336ea77
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-12 17:30:16 +0100

    by gacp

--- Module-08/ex01/main.cpp
+++ Module-08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:11 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/12 17:30:08 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/12 17:30:09 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int main(int you, char **nes)
 	
 	try
 	{
-		Span a(5);
+		Span a(2);
 		a.arrayFiller();
 		a.displayArray();
 		a.addNumber(1999);
