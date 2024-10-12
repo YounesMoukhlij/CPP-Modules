78bf2171f7f9c5337987afddb474310737012179
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-12 14:28:49 +0100

    by gacp

--- Module-07/ex02/Array.hpp
+++ Module-07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:40:35 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/12 14:28:41 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/12 14:28:46 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,7 +87,7 @@ Array<T>& Array<T>::operator=(const Array &origine)
 				_array[i] = origine._array[i];
 		}
 		else
-
+			_array = 0x0;
 	}
 	return (*this);
 }
