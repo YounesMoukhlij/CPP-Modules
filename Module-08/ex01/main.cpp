8fb8e543a2e9f4c8097c132cca66a79a9e6332f0
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-12 17:57:55 +0100

    by gacp

--- Module-08/ex01/main.cpp
+++ Module-08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:11 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/12 17:57:43 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/12 17:57:52 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ int main(int you, char **nes)
 		std::cerr << "The Program Does not take any Arguments." << std::endl;
 		return (EXIT_FAILURE);
 	}
-
+	
 	try
 	{
-	   span.addNumber(1);
+		span.addNumber(1);
         span.addNumber(2);
         span.addNumber(3);
         span.addNumber(4);
@@ -31,6 +31,23 @@ int main(int you, char **nes)
 
         // This should throw an exception since we are exceeding the maximum size
         span.addNumber(6); // Attempt to add a sixth number
+	}
+	try
+	{
+		Span A(12);
+		Span B(12);
+		
+		A.arrayFiller();
+		B = A;
+		// std::cout << "Displaying B" << std::endl;
+		// B.displayArray();
+		std::cout << "Displaying A" << std::endl;
+		A.displayArray();
+		A.addNumber(1999);
+		std::cout << "longestSpan : " << A.longestSpan() << std::endl;
+		std::cout << "shortestSpan : " << A.shortestSpan() << std::endl;
+		std::cout << "Displaying A" << std::endl;
+		// A.displayArray();
 	}
 	catch (std::exception &e)
 	{
