423c5a9ff51ea081ef3194e54863155befc447f0
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-12 17:24:55 +0100

    by gacp

--- Module-08/ex01/Span.cpp
+++ Module-08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/12 17:24:29 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/12 17:24:53 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,16 +33,7 @@ Span::Span(const Span& origine)
 	std::cout << "Copy Constructor Called." << std::endl;
 	_array = NULL;
 	if (this != &origine)
-	{
 		*this = origine;
-		_n = origine._n;
-		_i = origine._i;
-		_array = new int[_n];
-		if (!_array)
-			throw std::bad_alloc();
-		for (std::size_t i = 0x0; i < _n ; i++)
-			_array[i] = origine._array[i];
-	}
 }
 
 Span::~Span()
