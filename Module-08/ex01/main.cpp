95051acceb4ee81df3368865697ae67b7525f58b
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-12 17:33:44 +0100

    by gacp

--- Module-08/ex01/main.cpp
+++ Module-08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:11 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/12 17:33:05 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/12 17:33:43 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int main(int you, char **nes)
 	
 	try
 	{
-		Span A(2);
+		Span A();
 		Span B(12);
 		
 		A.arrayFiller();
