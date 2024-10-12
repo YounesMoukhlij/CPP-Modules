407104d79d04b1f082d61df502f6104653d615d8
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-12 14:24:41 +0100

    by gacp

--- Module-07/ex02/main.cpp
+++ Module-07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:42:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/12 14:23:27 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/12 14:24:41 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ int main(int you, char **nes)
 	B = A;
 	std::cout << B.getSize() << std::endl;
 	
-	T* Y = B.getArray();
 	for (int i = 0; i < B.getSize() ; i++)
 	{
 		std::cout <<  Y[i] << std::endl;
