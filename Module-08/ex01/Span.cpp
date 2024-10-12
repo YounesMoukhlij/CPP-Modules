b66eeada0920e24a5e9fb5558995b17893863411
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-12 17:23:56 +0100

    by gacp

--- Module-08/ex01/Span.cpp
+++ Module-08/ex01/Span.cpp
@@ -6,18 +6,16 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/12 17:23:46 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/12 17:23:54 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
 
-Span::Span() : _i(0x0), _n()
+Span::Span() : _i(0x0), _n(0x0), _array(0x0)
 {
 	_i = 0x0;
-	_array = NULL;
-	_n = 0x0;
 	std::cout << "Default Constructor Called." << std::endl;
 }
 
