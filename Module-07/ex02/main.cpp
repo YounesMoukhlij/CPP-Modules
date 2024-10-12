ad0aebfe1bbbb26f5a964b2c2ff8b0fc41ca71a6
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-12 14:21:14 +0100

    by gacp

--- Module-07/ex02/main.cpp
+++ Module-07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:42:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/12 14:14:10 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/12 14:21:13 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,9 @@ int main(int you, char **nes)
 	
 	// std::cout << B.getSize() << std::endl;
 	B = A;
-	// std::cout << B.getSize() << std::endl;
+	std::cout << B.getSize() << std::endl;
+	
+	while (i )
 	
 	// try
 	// {
