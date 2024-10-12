e285c21c1c755d5e8a8a5eeae4b891a299a2e2c5
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-12 14:12:56 +0100

    by gacp

--- Module-07/ex02/main.cpp
+++ Module-07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:42:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/12 14:12:46 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/12 14:12:56 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int main(int you, char **nes)
 	}
 	
 	Array<int> A(2);
-	Array<int> B(3);
+	Array<int> B(46);
 	
 	A = B;
 	std::cout << A.getSize() << std::endl;
