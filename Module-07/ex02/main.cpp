35aac970075b49c48bdfbea0794078a363410ee0
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-12 14:13:58 +0100

    by gacp

--- Module-07/ex02/main.cpp
+++ Module-07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:42:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/12 14:13:24 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/12 14:13:57 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,14 @@ int main(int you, char **nes)
 		return (EXIT_FAILURE);
 	}
 	
-	Array<int> A(2);
-	Array<int> B(466);
 	
-	std::cout << B.getSize() << std::endl;
-	B = A;
-	std::cout << B.getSize() << std::endl;
+	// Si
+	// Array<int> A(2);
+	// Array<int> B(466);
+	
+	// std::cout << B.getSize() << std::endl;
+	// B = A;
+	// std::cout << B.getSize() << std::endl;
 	
 	// try
 	// {
