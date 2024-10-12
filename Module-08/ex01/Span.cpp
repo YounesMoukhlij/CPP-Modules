b4964dbb448b1ed6020e887ddddf589f4eaeb6ef
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-12 18:00:58 +0100

    by gacp

--- Module-08/ex01/Span.cpp
+++ Module-08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:32:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/12 17:59:54 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/12 18:00:54 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,7 +102,6 @@ void	Span::arrayFiller()
 	{
 		int _value = std::rand() % 1000;
 		addNumber(_value);
-		// _array[index] = _value;
 	}
 	_i = _n;
 }
