071cb8857930beb4293e0cd2817552d8932456c8
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-12 14:04:10 +0100

    by gacp

--- Module-07/ex02/main.cpp
+++ Module-07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:42:01 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/11 14:42:14 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/12 14:04:10 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	younes()
 int main(int you, char **nes)
 {
 	atexit(younes);
+
 	IGNORE(nes);
 	if (you != 0x1)
 	{
