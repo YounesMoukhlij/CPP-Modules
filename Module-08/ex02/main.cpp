d1a29df8b30aab8f0d4af0b1f5e25ad11d715bd2
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-13 16:21:55 +0100

    by gacp

--- Module-08/ex02/main.cpp
+++ Module-08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 13:42:32 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/13 13:55:33 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/13 16:21:52 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,8 @@ int main(int you, char **nes)
 	std::cout << "SIZE After: " << mstack.size() << std::endl;
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
+	MutantStack<int>::const_ it = mstack.begin();
+	MutantStack<int>::const_ ite = mstack.end();
 	++it;
 	--it;
 	while (it != ite)
