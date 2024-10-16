e8952b1a0972aabd3dff152ee0ae489a6929f60c
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-16 18:07:15 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/16 18:06:47 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/16 18:07:12 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -147,8 +147,6 @@ void	RPN::CalculateStaff(int opr, std::stack<int>& _stack)
 	int	Y;
 	int	num1;
 	int	num2;
-	(void ) Y;
-	
 	
 	// if operator then do simple math
 	if (_stack.size() < 0x2)
