a26d8fde090017d20ede07d44bbbf8d1f7754550
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-16 17:33:05 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/16 17:32:52 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/16 17:33:01 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -220,7 +220,7 @@ void	RPN::PolonaiseInverse(const std::string& _array)
 	{
 		// std::cout << "readed str := " << read << std::endl;
 		// std::cout << "readed read[0] := " << read[0x0] << std::endl;
-		pri
+		std::cout << "read" 
 		if (isdigit(read[0x0]))
 		{
 			/*
