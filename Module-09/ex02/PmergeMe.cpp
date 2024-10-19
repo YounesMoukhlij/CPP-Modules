d0ad97f200a3e20a9e15800616768240979ae511
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 15:37:35 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,9 +6,43 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 15:37:13 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 15:37:27 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
+PmergeMe::PmergeMe()
+{
+	std::cout << "constructor by default called" << std::endl;
+}
+
+PmergeMe::PmergeMe(const PmergeMe& origine)
+{
+	std::cout << "Copy Constructor Called" << std::endl;
+	if (this != &origine)
+		*this = origine;
+}
+
+PmergeMe::~PmergeMe()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+PmergeMe&	PmergeMe::operator=(const PmergeMe& origine)
+{
+	if (this != &origine)
+    {
+	}
+	return (*this);
+}
+
+const char* PmergeMe::InvalidData::what() const throw()
+{
+	return ("Invalid argument!");
+}
+
+const char* PmergeMe::DuplicateData::what() const throw()
+{
+	return ("Duplicate Number!");
+}
--- Module-09/ex02/PmergeMe.hpp
+++ Module-09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 15:37:18 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 15:37:18 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 15:37:25 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,37 +48,3 @@ class PmergeMe
 };
 
 
-PmergeMe::PmergeMe()
-{
-	std::cout << "constructor by default called" << std::endl;
-}
-
-PmergeMe::PmergeMe(const PmergeMe& origine)
-{
-	std::cout << "Copy Constructor Called" << std::endl;
-	if (this != &origine)
-		*this = origine;
-}
-
-PmergeMe::~PmergeMe()
-{
-	std::cout << "Destructor called" << std::endl;
-}
-
-PmergeMe&	PmergeMe::operator=(const PmergeMe& origine)
-{
-	if (this != &origine)
-    {
-	}
-	return (*this);
-}
-
-const char* PmergeMe::InvalidData::what() const throw()
-{
-	return ("Invalid argument!");
-}
-
-const char* PmergeMe::DuplicateData::what() const throw()
-{
-	return ("Duplicate Number!");
-}
