f5a0382db2fb0981c00e12cdf7661dbfc4da61b7
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 16:22:26 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 16:22:10 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 16:22:22 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,6 @@ void PmergeMe::OPerationParse(const std::string& Data_Entry)
 		if (isdigit(Data_Entry[i]))
 			counter++;
 		i++;
-		puts("YOUNES");
 	}
 	if (!counter)
 		throw InvalidData();
