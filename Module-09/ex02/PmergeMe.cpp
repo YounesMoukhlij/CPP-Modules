b520a76090c914a9c632baa1d9de462171a08aeb
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 15:34:54 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,10 +6,13 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 15:34:42 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 15:34:53 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-class
+class PmergeMe
+{
+	private:
+};
