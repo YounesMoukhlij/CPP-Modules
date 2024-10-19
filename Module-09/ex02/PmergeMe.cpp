49a097f9e6ed8655583b9c9ca14884c3d4af8cd4
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 15:53:28 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 15:53:17 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 15:53:26 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,4 +47,4 @@ const char* PmergeMe::DuplicateData::what() const throw()
 	return ("Duplicate Number!");
 }
 
-void PmergeMe::OPerationParse
+void PmergeMe::OPerationParse(const std::string)
