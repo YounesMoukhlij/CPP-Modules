3161b9cb877d18e3b413375f796006cd80fd82da
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 16:37:00 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 16:36:48 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 16:36:53 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ PmergeMe::PmergeMe(const PmergeMe& origine)
 
 PmergeMe::~PmergeMe()
 {
-	std::cout << ".. Destructor called" << std::endl;
+	std::cout << "... Destructor called ..." << std::endl;
 }
 
 PmergeMe&	PmergeMe::operator=(const PmergeMe& origine)
