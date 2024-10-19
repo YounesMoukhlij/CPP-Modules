375666be4fa4e6443b0215dff3164f694cbbf25b
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-19 16:08:23 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/19 16:04:41 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/19 16:08:21 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,13 @@ void PmergeMe::OPerationParse(const std::string& Data_Entry)
 
 	countDigit = 0;
 	size_t	i = 0;
+	for (size_t i = 0; i < Data_Entry.size(); i++)
+	{
+		if (Data_Entry[i] == '+' && i == 0)
+			i++;
+		else
+			break;
+	}
 	while (i < Data_Entry.size())
 	{
 		if (isspace(Data_Entry[i]))
