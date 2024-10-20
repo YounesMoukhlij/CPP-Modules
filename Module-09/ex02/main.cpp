cb54b3e3531fb6ef34f19dc115f309171d8660f8
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 15:48:47 +0100

    by gacp

--- Module-09/ex02/main.cpp
+++ Module-09/ex02/main.cpp
@@ -16,7 +16,7 @@ int main(int you, char **nes)
 		for (int i = 1; i < you; i++)
 			OPerationAPPOLLO.OPerationParse(static_cast<std::string>(nes[i]));
 		std::cout << "~ Before ~" << std::endl;
-		OPerationAPPOLLO.OPerationPrint();
+		// OPerationAPPOLLO.OPerationPrint();
 		OPerationAPPOLLO.FordJhonsonSort();
 		// std::cout << "~ After ~" << std::endl;
 		// OPerationAPPOLLO.OPerationPrint();
