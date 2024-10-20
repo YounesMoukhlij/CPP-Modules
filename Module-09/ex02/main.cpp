af9de565898f201a4f8d10b5b40c6f49d07126a8
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 16:45:33 +0100

    by gacp

--- Module-09/ex02/main.cpp
+++ Module-09/ex02/main.cpp
@@ -18,7 +18,7 @@ int main(int you, char **nes)
 		std::cout << "~ Before ~" << std::endl;
 		OPerationAPPOLLO.OPerationPrint();
 
-		OPerationAPPOLLO.O();
+		OPerationAPPOLLO.OP();
 
 		std::cout << "~ After ~" << std::endl;
 		OPerationAPPOLLO.OPerationPrint();
