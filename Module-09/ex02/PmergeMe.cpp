0217c08089ab3444d04607b9e9d328c7e2b3e198
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 12:10:58 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 12:10:46 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 12:10:54 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,8 +95,7 @@ void	PmergeMe::FordJhonsonSort(std::vector<int> &Data)
 {
 	std::vector<std::pair<int, int> > Pairs;
 	
-		std::vector<int>::iterator	it;
-	it = _vector.begin();
+	std::vector<int>::iterator	it = _vector.begin();
 	while (it != _vector.end())
 	{
 		_pairVector.insert(_pairVector.end(), std::make_pair(*it, *(it + 1)));
