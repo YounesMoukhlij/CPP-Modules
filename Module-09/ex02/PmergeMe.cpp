d539886fe6306b911373975145bfc06feedaa38d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 17:03:49 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 17:03:20 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 17:03:44 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -170,6 +170,6 @@ void	PmergeMe::OPerationSort()
 void	PmergeMe::OPerationTime()
 {
 	std::cout << "\033[41m ^_* TIME Difference ^_*\033[0m" << std::endl;
-	std::cout << "Time for Vector : " << getTime(startVector, endVector) << std::endl;
+	std::cout << "Time for Vector : " << getTime(startVector, endVector) << << std::endl;
 	std::cout << "Time for Deque : " << getTime(startDeque, endDeque) << std::endl;
 }
