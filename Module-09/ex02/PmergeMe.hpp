ed17e707dae4e868c11f80122731a466bb6a6d77
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 12:30:28 +0100

    by gacp

--- Module-09/ex02/PmergeMe.hpp
+++ Module-09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 15:37:18 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 12:30:18 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 12:30:24 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@
 class PmergeMe
 {
     private:
-		std::vector<int>	_vector;
 		size_t				_size;
-		
+		std::vector<int>	_vector;
+		int					poped_value;
 	public:
 		// Canonical Form :
 		// Constructor
