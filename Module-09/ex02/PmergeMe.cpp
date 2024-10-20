d72de6d23effe7967966b704de1a03d187cb9d32
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 12:51:25 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 12:47:55 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 12:51:18 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,7 +110,7 @@ void	PmergeMe::FordJhonsonSort(void)
 		(it1->first > it1->second) ? std::swap(it1->first, it1->second) : void();
 	// Sort All the pairs 
 	std::sort(Pairs.begin(), Pairs.end());
-		
+		 
 	printPairs(Pairs);
 }
 
