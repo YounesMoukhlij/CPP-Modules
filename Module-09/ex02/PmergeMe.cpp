c38eb3f7a2e3c7bdd2d91bd622385290df4953da
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 12:32:15 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 12:32:05 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 12:32:14 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,13 +93,14 @@ void	PmergeMe::OPerationPrint() const
 
 void	PmergeMe::FordJhonsonSort(void)
 {
+	in
 	std::vector<std::pair<int, int> >::iterator	it1;
 	std::vector<std::pair<int, int> >			Pairs;
 	std::vector<int>::iterator					it = _vector.begin();
 	
 	if (_vector.size() % 0x2)
 	{
-		(1) & (poped_value = *(--(_vector.end())), _vector.pop_back(), random << );
+		(1) & (poped_value = *(--(_vector.end())), _vector.pop_back(), random << 0x1);
 		std::cout << "LAST VALUE : " << poped_value << std::endl;
 	}
 	while (it != _vector.end())
