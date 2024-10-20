0d5a5cf5c04d537ee0654cb9954a9438044d9965
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 17:54:49 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 17:54:35 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 17:54:43 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,7 +83,7 @@ void	PmergeMe::OPerationPrint(int mode1, int mode2) const
 {
 	size_t i = 0x0;
 	
-	(mode2 == BEFORE) ? std::cout << "~ Before ~" << std::endl;
+	(mode2 == BEFORE) ? std::cout << "~ Before ~" << std::endl : std::cout << "";
 	else if (mode2 == AFTER)
 		std::cout << "~ After ~" << std::endl;
 	
