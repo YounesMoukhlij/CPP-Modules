aecdd140ed37bf6132cdbfc633047c09f3645eb6
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 17:47:58 +0100

    by gacp

--- Module-09/ex02/main.cpp
+++ Module-09/ex02/main.cpp
@@ -5,7 +5,7 @@ int main(int you, char **nes)
 {
     if (you == 0x1)
 	{
-		std::cerr << "Error Try ./PmergeMe <arg1> <arg2> ..." << std::endl;
+		std::cerr << "Error Try ./PmergeMe <arg1> <arg2> ..." << std::endl
 		return (EXIT_FAILURE);
 	}
 
