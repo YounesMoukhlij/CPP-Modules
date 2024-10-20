c7bf86becda7547e18ea47070d4181c86598e84e
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 15:46:29 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 15:10:39 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 15:46:27 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,6 +124,6 @@ void PmergeMe::printPairs(const std::vector<std::pair<int, int> >& Pairs)
 {
 	std::cout << "priting pairs" << std::endl;
     for (std::vector<std::pair<int, int> >::const_iterator it = Pairs.begin(); it != Pairs.end(); ++it) {
-        std::cout << "(" << it->first << ", " << it->second << ")\n";
+        std::cout << "(" << it->first << ", " << it->second << ")" << std::endl;
     }
 }
