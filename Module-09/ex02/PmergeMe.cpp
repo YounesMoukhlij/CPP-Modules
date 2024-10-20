fab624d5df37deab1bf2f5c1a92be7a0d8da6ebd
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 16:55:12 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 16:54:48 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 16:55:06 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -156,8 +156,6 @@ void	PmergeMe::FordJhonsonDequeSort(void)
 
 void	PmergeMe::OPerationSort()
 {
-	struct timeval startVector, endVector;
-	struct timeval startDeque, endDeque;
 	
 	gettimeofday(&startVector, NULL);
 	FordJhonsonVectorSort();
