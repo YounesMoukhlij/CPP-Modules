3862fa6db81bc8615f9e96e95a64bd602818edb4
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 17:25:11 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 17:24:44 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 17:25:10 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -212,7 +212,7 @@ void	PmergeMe::OPerationSort()
 	
 
 	// gettimeofday(&startDeque, NULL);
-	FordJhonsonDequeSort();
+	// FordJhonsonDequeSort();
 	// gettimeofday(&endDeque, NULL);
 }
 
