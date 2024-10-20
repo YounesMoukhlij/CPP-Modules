a2fcc7f91da5d9790d79da45df4cd0836c6a6da6
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 12:31:55 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 12:31:38 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 12:31:45 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,8 +99,7 @@ void	PmergeMe::FordJhonsonSort(void)
 	
 	if (_vector.size() % 0x2)
 	{
-		(poped_value = *(--(_vector.end()));
-		_vector.pop_back();
+		(1) & (poped_value = *(--(_vector.end())), _vector.pop_back());
 		std::cout << "LAST VALUE : " << poped_value << std::endl;
 	}
 	while (it != _vector.end())
