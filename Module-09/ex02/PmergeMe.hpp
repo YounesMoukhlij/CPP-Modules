e15beef43505ef28f3bd1e0416290d7e29f47c2e
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 16:53:26 +0100

    by gacp

--- Module-09/ex02/PmergeMe.hpp
+++ Module-09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 15:37:18 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 16:53:11 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 16:53:13 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@
 class PmergeMe
 {
     private:
-		std::vector<int>							_vector;
 		std::vector<std::pair<int, int> >			Pairs;
+		std::vector<int>							_vector;
 		int											poped_value;
 		struct timeval 								startDeque, endDeque;
 		struct timeval 								startVector, endVector;
