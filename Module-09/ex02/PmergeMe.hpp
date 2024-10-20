e8adc49d8d51eb26372e10031dc4ed9e98bcb590
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 17:05:00 +0100

    by gacp

--- Module-09/ex02/PmergeMe.hpp
+++ Module-09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 15:37:18 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 17:04:47 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 17:04:59 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,9 +50,9 @@ class PmergeMe
 		void			OPerationPrint(void) const;
 		void			FordJhonsonDequeSort(void);
 		void			FordJhonsonVectorSort(void);
-		double			PmergeMe::getTime(const struct timeval& start, const struct timeval& end)
 		
 		void			OPerationParse(const std::string &Data_Entry);
+		double			getTime(const struct timeval& start, const struct timeval& end);
 		// void			OPerationPrint(std::vector<int>& Data) const;
 		// void 			printPairs(const std::vector<std::pair<int, int> >& Pairs);
 		
