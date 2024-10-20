deeec40fcf9fa4f53598705bd4da5dc53ea77b13
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 17:29:07 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 17:27:23 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 17:29:07 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -165,7 +165,7 @@ void	PmergeMe::FordJhonsonDequeSort(void)
 		deque_value = *(--(_deque.end())), _deque.pop_back();
 		
 	while (it != _deque.end())
-		Pairs.insert(Pairs.end(), std::make_pair(*it, *(it + 0x1))), it += 0x2;
+		Pairs_Deque.insert(Pairs_Deque.end(), std::make_pair(*it, *(it + 0x1))), it += 0x2;
 	
 
 	// Sort the pairs 
