2750faf44ac184cc30db66e7d67535fe39a49bed
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 17:24:52 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 17:24:35 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 17:24:44 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -133,7 +133,7 @@ void	PmergeMe::FordJhonsonVectorSort(void)
 	{
 		_vector.push_back(*it);
 		if (it + 0x1 == Final_Vector.end())
-			LastElement = std::upper_bound(_vector.begin(), _vector.end(), poped_value), _vector.insert(LastElement, poped_value);
+			LastElement = std::upper_bound(_vector.begin(), _vector.end(), vector_value), _vector.insert(LastElement, vector_value);
 		it++;			
 	}
 	
--- Module-09/ex02/PmergeMe.hpp
+++ Module-09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 15:37:18 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 17:23:38 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 17:24:51 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,8 @@ class PmergeMe
 		std::vector<std::pair<int, int> >			Pairs;
 		std::deque<int>								_deque;
 		std::vector<int>							_vector;
-		int											poped_value;
+		int											vector_value;
+		int											vector_value;
 		// struct timeval 								startDeque, endDeque;
 		struct timeval 								startVector, endVector;
 		
