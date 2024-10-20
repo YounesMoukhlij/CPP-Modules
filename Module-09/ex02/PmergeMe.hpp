a3ab727c2eb953ef374b4aec652075c5f88a13c9
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 16:53:37 +0100

    by gacp

--- Module-09/ex02/PmergeMe.hpp
+++ Module-09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 15:37:18 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 16:53:13 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 16:53:33 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,7 @@ class PmergeMe
 		// Functions Member
 		void			OPerationPrint() const;
 		void			OPerationSort(int mode);
+		void			OPerationTime();
 		void			FordJhonsonDequeSort(void);
 		void			FordJhonsonVectorSort(void);
 		void			OPerationParse(const std::string &Data_Entry);
