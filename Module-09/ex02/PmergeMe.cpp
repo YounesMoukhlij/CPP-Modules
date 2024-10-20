9f54e2212d5733c124fac60800b32d43737a38ed
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-20 12:32:37 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 12:32:22 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/20 12:32:35 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,6 +116,7 @@ void	PmergeMe::FordJhonsonSort(void)
 	// 	it1++;
 	// }
 	printPairs(Pairs);
+	ra
 }
 
 void	PmergeMe::OPerationSort()
