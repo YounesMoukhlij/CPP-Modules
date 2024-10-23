0e1ecf4964ecd564e33e1e8325d91d3519b1ee7d
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-23 11:50:30 +0100

    commit with gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -37,7 +37,7 @@ PmergeMe&	PmergeMe::operator=(const PmergeMe& origine)
 		_deque = origine._deque;
 		Pairs = origine.Pairs;
 		Pairs_Deque = origine.Pairs_Deque;
-		
+		st
 	}
 	return (*this);
 }
