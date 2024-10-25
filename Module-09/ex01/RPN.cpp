e2d7d66ed21562b37ac0de2daeb34ac998d5bc78
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-25 16:58:54 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/25 16:58:40 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/25 16:58:53 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -186,7 +186,6 @@ void	RPN::CalculateStaff(int opr, std::stack<int>& _stack)
 		default :
 			throw ErrorArgument();
 	}
-	// std::cout << "REEEE :: " << _stack.top() << std::endl;
 }
 
 void	RPN::PolonaiseInverse(const std::string& _array)
