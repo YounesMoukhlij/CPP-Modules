6c44ebd6f3c97eade44fb1aeb7ce19bc42a3f134
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-25 14:58:07 +0100

    by gacp

--- Module-09/ex00/main.cpp
+++ Module-09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:40:33 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/25 14:57:51 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/25 14:57:58 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,19 +36,19 @@ int main(int you, char **nes)
 		std::cerr << younes.what() << std::endl;
 	}
 	
-	std::map<std::string, float>	data;
+	// std::map<std::string, float>	data;
 
-	data.insert(std::make_pair("2024-10-25", 0x1));
-	data.insert(std::make_pair("2024-11-25", 0x2));
-	data.insert(std::make_pair("2024-09-25", 0x3));
+	// data.insert(std::make_pair("2024-10-25", 0x1));
+	// data.insert(std::make_pair("2024-11-25", 0x2));
+	// data.insert(std::make_pair("2024-09-25", 0x3));
 	
 
-	std::map<std::string, float>::const_iterator it = data.begin();
+	// std::map<std::string, float>::const_iterator it = data.begin();
 
-	for (it = data.begin(); it != data.end(); it++)
-	{
-		std::cout << it->first << " => " << it->second << std::endl;
-	}
+	// for (it = data.begin(); it != data.end(); it++)
+	// {
+	// 	std::cout << it->first << " => " << it->second << std::endl;
+	// }
 	
 	return (EXIT_SUCCESS);
 }
