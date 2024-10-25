9101ea6a7c02f2a67c978b66eabb0e278393ab62
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-25 15:39:14 +0100

    by gacp

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:39:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/25 15:34:29 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/25 15:39:13 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -209,7 +209,10 @@ bool	BitcoinExchange::parseEntry(std::string& str)
 		throw InvalidData();
 	}
 	if (value > INT_MAX)
+	{
+		puts("Error: too large a number.");
 		return ((std::cerr << "Error: too large a number." << std::endl), false);
+	}
 	if (value < 0x0)
 		return ((std::cerr << "Error : not a positive number." << std::endl), false);
 	return (true);
