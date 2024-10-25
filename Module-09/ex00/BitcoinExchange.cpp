cff870309321bb4c9c4fb38836c930b0b550dc7c
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-25 12:57:09 +0100

    by gacp

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:39:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/25 12:56:53 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/25 12:57:09 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -148,7 +148,7 @@ void	BitcoinExchange::printData(const std::string& inputDate)
 	
 	// for (it = dataMap.begin(); it != dataMap.end(); ++it)
 	// 	std::cout << it->first << " => " << it->second << std::endl;
-	float value = dataMap["2016-12-27"];
+	float value = dataMap["2022-03-29"];
 	std::cout << "value :"<< value << std::endl;
 	// it = dataMap.lower_bound(inputDate);
 	// if (it != dataMap.end() && it->first == inputDate)
