8712c8ae476ec730f068e3cf0848710372d412a6
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-25 13:03:45 +0100

    by gacp

--- Module-09/ex00/main.cpp
+++ Module-09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:40:33 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/25 13:03:36 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/25 13:03:39 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,8 @@ int main(int you, char **nes)
 	std::map<std::string, float>	data;
 
 	data.insert(std::make_pair("2024-10-25", 0x1));
-	data.insert(std::make_pair("2024-10-25", 0x2));
-	data.insert(std::make_pair("2024-10-25", 0x1));
+	data.insert(std::make_pair("2024-11-25", 0x2));
+	data.insert(std::make_pair("2024-12-25", 0x3));
 	
 
 	std::map<std::string, float>::const_iterator it = data.begin();
