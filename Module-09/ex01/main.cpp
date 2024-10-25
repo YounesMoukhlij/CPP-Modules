ac4ddbc01f13bb584579431b7e3ff5f9d86f4a4d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-25 17:08:25 +0100

    by gacp

--- Module-09/ex01/main.cpp
+++ Module-09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:43:23 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/25 16:57:53 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/25 17:08:23 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int main(int you, char **nes)
 	try
 	{
 		RPN	rpn;
-		RPN	rpn1;
+		RPN	rpn1(rpn);
 
 		
 		std::string	data = static_cast<const std::string>(nes[0x1]);
