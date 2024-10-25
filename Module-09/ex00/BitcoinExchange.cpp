2f375d933ee76213916fcd579f4c812a0b79e916
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-25 12:15:56 +0100

    by gacp

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:39:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/25 12:15:47 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/25 12:15:50 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,6 @@ BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& origine)
 		dataMap = origine.dataMap;
 		infile = origine.infile;
 		dataBase = origine.dataBase;
-		
 	}
 	return (*this);
 }
