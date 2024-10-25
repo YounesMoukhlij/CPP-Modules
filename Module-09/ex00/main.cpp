064b813c06fa9e48d7d86f4f683e3760e5f512ae
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-25 13:03:21 +0100

    by gacp

--- Module-09/ex00/main.cpp
+++ Module-09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:40:33 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/25 13:03:10 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/25 13:03:14 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,10 @@ int main(int you, char **nes)
 
 	std::map<std::string, float>::const_iterator it = data.begin();
 
-	for (it )
+	for (it = data.begin(); it != data.end(); it++)
+	{
+		std::cout << it->first << " => " << it->second << std::endl;
+	}
 	
 	return (EXIT_SUCCESS);
 }
