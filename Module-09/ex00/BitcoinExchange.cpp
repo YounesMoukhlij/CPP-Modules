92d5d2cc6c30a5a3eb4649d000121db101995e9a
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-25 11:58:03 +0100

    by gacp

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:39:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/25 11:57:55 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/25 11:58:03 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,7 +76,7 @@ void	BitcoinExchange::openingFiles(const std::string& name, int j)
 	}
 	dataBase.open("data.csv");
 	if (!dataBase.is_open())
-		(1) & (infile.close(), std::cerr << "data.csv: ", throw Invalidfile(), j = 0x2);
+	{infile.close(), std::cerr << "data.csv: ", throw Invalidfile();
 }
 
 
