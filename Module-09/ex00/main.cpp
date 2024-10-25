62c86caa23b64de41f3340f28c31cad4b8536358
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-25 16:31:48 +0100

    by gacp

--- Module-09/ex00/main.cpp
+++ Module-09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:40:33 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/25 16:31:36 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/25 16:31:42 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int main(int you, char **nes)
 		bit.readData();
 		bit.fileDescriptors();
 		
-		a
+		a = bit;
 	}
 	catch (std::exception &younes)
 	{
