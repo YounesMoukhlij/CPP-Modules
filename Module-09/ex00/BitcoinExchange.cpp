7ad6bcf72e4318346ca22e19da5876210a3438e9
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-26 13:56:45 +0100

    by gacp

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:39:04 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/26 13:56:26 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/26 13:56:44 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -180,8 +180,6 @@ bool	BitcoinExchange::parseEntry(std::string& str)
 	delPos = str.find('|');
 	if (delPos == std::string::npos)
 		return ((std::cerr << "Error : bad input => " << str << std::endl), false);
-	if (!str.size())
-		return ((std::cerr << "Error : bad input (Please Enter Data)" << str << std::endl), false);
 	date = trim(str.substr(0x0, delPos));
 	strV = trim(str.substr(delPos + 0x1));
 	// check dates
