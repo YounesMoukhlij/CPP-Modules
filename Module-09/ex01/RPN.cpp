ae55228735d17ba4e7f012f3ee3f7d562ab7a35f
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-26 14:11:21 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/26 14:11:08 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/26 14:11:14 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -239,8 +239,8 @@ void	RPN::ParseArguments(const std::string& _array) const
 	}
 	if (Cnumbers < 0x2)
 		throw SmallStack();
-	// if (Coperators + 0x1 != Cnumbers)
-	// 	throw ErrorArgument();
+	if (Coperators + 0x1 != Cnumbers)
+		throw ErrorArgument();
 }
 
 
