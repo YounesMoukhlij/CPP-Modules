d777570d7809866f79a1fe5d5a1c619a6c54f296
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-26 17:52:27 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/26 17:52:13 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/26 17:52:21 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -287,6 +287,7 @@ std::string	formal_string(const std::string& _array)
 		{
 			new_array += _array[i];
 		}
+		i++;
 	}
 	return (new_array);
 }
