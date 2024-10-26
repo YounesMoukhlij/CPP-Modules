a1a41969ad1b7b90843c2441aa6a2d1095d3b10d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-26 17:37:00 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/26 17:36:44 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/26 17:36:57 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -200,14 +200,11 @@ void	RPN::PolonaiseInverse(const std::string& _array)
 	while (std::getline(str, read, ' '))
 	{
 		if (isdigit(read[0x0]))
-		{
-			puts("2222");
 			_stack.push(IntConversion(read));
-		}
 		else
 		{
 			CalculateStaff(OperatorChecker(read), _stack);
-			
+		}
 	}
 	std::cout << "Result : " << _stack.top() << std::endl;
 }
