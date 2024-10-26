c3eaedb197472fe62882797f408f1f072af97425
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-26 14:28:49 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/20 17:57:06 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/26 14:28:48 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -130,6 +130,8 @@ void	PmergeMe::FordJhonsonVectorSort(void)
 	// The Last Element Make noises it's better to pop it
 	if (_vector.size() % 0x2)
 		vector_value = *(--(_vector.end())), _vector.pop_back();
+	
+	
 
 	while (it != _vector.end())
 		Pairs.insert(Pairs.end(), std::make_pair(*it, *(it + 0x1))), it += 0x2;
