c343ac0fb38691dd3e72cdc67732d83f06cbaec1
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-26 14:03:04 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/26 14:02:51 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/26 14:03:01 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -215,17 +215,17 @@ void	RPN::ParseArguments(const std::string& _array) const
 	size_t	Cnumbers = length - _array.size();
 	size_t	Coperators = Cnumbers + 0x0;
 	
-	// if (!isdigit(static_cast<int>(_array[0x0])))
-	// 	throw ErrorArgument();
-	// if (!ValidArg(_array[_array.size() - 0x1]) && _array[_array.size() - 0x1] != ' ')
-	// 	throw ErrorArgument();
+	if (!isdigit(static_cast<int>(_array[0x0])))
+		throw ErrorArgument();
+	if (!ValidArg(_array[_array.size() - 0x1]) && _array[_array.size() - 0x1] != ' ')
+		throw ErrorArgument();
 	for (static size_t i; i < length; i++)
 	{
 		if (!isdigit(_array[i]) && !ValidArg(_array[i]))
 			throw ErrorArgument();
 		if (ValidArg(_array[i]) && _array[i] != ' ')
 			Coperators++;
-		if (isdigit(_array[i]) && _array[i] != ' ')
+		if (isdigit(_array[i]) && _array[i] != ' ' || )
 			Cnumbers++;
 	}
 	if (Cnumbers < 0x2)
