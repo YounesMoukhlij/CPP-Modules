bce212b1d858699831a6ce72d0d88b0ffe971abd
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-26 13:35:18 +0100

    by gacp

--- Module-09/ex00/main.cpp
+++ Module-09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:40:33 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/25 16:36:31 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/26 13:35:16 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,14 +32,6 @@ int main(int you, char **nes)
 		bit.loadData();
 		bit.readData();
 		bit.fileDescriptors();
-		
-		BitcoinExchange	a(bit);
-		
-		a.nameChecker(file);
-		a.openingFiles(file);
-		a.loadData();
-		a.readData();
-		a.fileDescriptors();
 	}
 	catch (std::exception &younes)
 	{
