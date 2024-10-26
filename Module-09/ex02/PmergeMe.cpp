968bf1b29798cba321eff01a47be75153dffefe5
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-26 16:47:58 +0100

    by gacp

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:56:14 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/26 16:47:10 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/26 16:47:56 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -162,7 +162,7 @@ void	PmergeMe::FordJhonsonVectorSort(void)
 		tmp_it++;
 	}
 	_vector.clear();
-	if (Final_Vector.size() == 0x1)
+	if (Final_Vector.begin() == Final)
 	{
 		puts("123123123123");
 		_vector.push_back(*it);
