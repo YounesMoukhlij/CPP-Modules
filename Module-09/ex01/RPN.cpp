049314c427be8798dddee44d18e9290187357fda
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-10-26 16:27:02 +0100

    by gacp

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:31:30 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/10/26 16:26:47 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/10/26 16:26:53 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -144,7 +144,7 @@ void	RPN::CheckFlow(int Value_1, int Value_2, char opr) const
 
 void	RPN::CalculateStaff(int opr, std::stack<float>& _stack)
 {
-	int	Y;
+	int		Y;
 	float	num1;
 	float	num2;
 	
